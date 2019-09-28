b1e06cb7ccab904405848d6cdc27fd4246a06142
Romez <Romez1990@gmail.com>
2019-09-28 22:17:08 +0300

    Refactor scraper logic

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -5,12 +5,12 @@ from bs4 import BeautifulSoup
 
 class Scraper:
     def scrap(self) -> List[str]:
-        html = requests.get('http://viti-mephi.ru/raspisanie').text
+        result = requests.get('http://viti-mephi.ru/raspisanie')
+        html = result.text
         soup = BeautifulSoup(html, 'lxml')
-        blok = soup.find(id="node-253")
 
         links: List[str] = []
-        for file in blok.find_all('span', class_='file'):
-            for link in file.find_all('a'):
-                links.append(link.get('href'))
+        link_elements = soup.select('#node-253 .file a')
+        for link in link_elements:
+            links.append(link.get('href'))
         return links
