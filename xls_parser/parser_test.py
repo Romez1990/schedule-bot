6c5828b4402e6e1b6b663e3fa8e33afa354c2d5e
Romez <Romez1990@gmail.com>
2019-10-07 06:55:36 +0300

    Write additional tests

--- xls_parser/parser_test.py
+++ xls_parser/parser_test.py
@@ -1,15 +1,12 @@
 from pathlib import Path
+from pytest import fixture
 from .parser import Parser
 from renderer import Renderer, Group, ScheduleKind
 
 
-def test_parser() -> None:
-    parser = Parser()
-    current_directory = Path(__file__).parent
-    file_path = current_directory / 'test_files' / \
-                'raspisanie_grupp_s_07_10_2019_0 (fourth year only).xlsx'
-    schedule = parser.parse(file_path)
-    renderer = Renderer()
+def test_render_our_groups(parser: Parser, part_of_file: Path,
+                           renderer: Renderer) -> None:
+    schedule = parser.parse(part_of_file)
     wanted_groups = [
         Group('4ПрИн-5.16'),
         Group('4ПрИн-5а.16'),
@@ -17,3 +14,38 @@ def test_parser() -> None:
     wanted_schedule = schedule.filter(wanted_groups)
     renderer.render(wanted_schedule, ScheduleKind.week)
     renderer.render(wanted_schedule, ScheduleKind.week, 'dark')
+
+
+def test_render_all_groups(parser: Parser, full_file: Path,
+                           renderer: Renderer) -> None:
+    schedule = parser.parse(full_file)
+    for group in schedule.groups.keys():
+        wanted_schedule = schedule.filter([group])
+        renderer.render(wanted_schedule, ScheduleKind.week)
+        renderer.render(wanted_schedule, ScheduleKind.week, 'dark')
+
+
+@fixture
+def full_file() -> Path:
+    current_directory = Path(__file__).parent
+    file_path = current_directory / 'test_files' / \
+                'raspisanie_grupp_s_07_10_2019_0.xlsx'
+    return file_path
+
+
+@fixture
+def part_of_file() -> Path:
+    current_directory = Path(__file__).parent
+    file_path = current_directory / 'test_files' / \
+                'raspisanie_grupp_s_07_10_2019_0 (fourth year only).xlsx'
+    return file_path
+
+
+@fixture
+def parser() -> Parser:
+    return Parser()
+
+
+@fixture
+def renderer() -> Renderer:
+    return Renderer()
