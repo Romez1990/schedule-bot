0c7d32eac031eba85d88f2c9bccce3615265ebc3
Iglek Kuat <dizinnes228@gmail.com>
2020-08-31 00:39:52 +0600

    dev: update greeting.py, add class Greeting and added handler for command `/start`

---
+++ telegram_bot/greeting.py
@@ -0,0 +1,29 @@
+from aiogram import Bot, Dispatcher, executor, types
+from asyncio import run
+
+import asyncio
+import logging
+import os
+
+API_TOKEN = os.getenv('API_KEY_SCHEDULE_BOT')
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
+# Initialize bot and dispatcher
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+
+class Greeting:
+    def __init__(self):
+        pass
+
+    @dp.message_handler(commands=['start'])
+    async def send_welcome(self, message: types.Message) -> None:
+        """
+        This handler will be called when user sends `/start`
+        :param message:
+        :return: None
+        """
+        await message.reply('Привет я бот')
