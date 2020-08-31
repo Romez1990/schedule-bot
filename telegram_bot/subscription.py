864fbd56fd2b91bbf58a493705a9d8bc8f5ef7f8
Iglek Kuat <dizinnes228@gmail.com>
2020-08-31 15:51:46 +0600

    refactor: change imports, put the libraries later their dependencies

--- telegram_bot/subscription.py
+++ telegram_bot/subscription.py
@@ -1,8 +1,9 @@
 from aiogram import executor, types
+from aiogram.types import ParseMode
+
 from telegram_bot.configurations.configure import dp
 from telegram_bot.configurations.messages_text import message_text_start, message_text_help
 from telegram_bot.configurations.button_configuration import buttons
-from aiogram.types import ParseMode
 
 import logging
 
