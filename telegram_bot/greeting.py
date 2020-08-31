53e8ff606cc2317a09d0d9ff1d15c20a7c7dc8aa
Iglek Kuat <dizinnes228@gmail.com>
2020-08-31 15:50:06 +0600

    refactor: change import Message

--- telegram_bot/greeting.py
+++ telegram_bot/greeting.py
@@ -1,8 +1,10 @@
-from aiogram import executor, types
+from aiogram import executor
+from aiogram.types import Message
+from aiogram.types import ParseMode
+
 from telegram_bot.configurations.configure import dp
 from telegram_bot.configurations.messages_text import message_text_start, message_text_help
 from telegram_bot.configurations.button_configuration import buttons
-from aiogram.types import ParseMode
 
 import logging
 
@@ -15,7 +17,7 @@ class Greeting:
         pass
 
     @dp.message_handler(commands=['start'])
-    async def send_welcome(message: types.Message) -> None:
+    async def send_welcome(message: Message) -> None:
         """
         This handler will be called when user sends `/start`
         :param message: types.Message
@@ -26,7 +28,7 @@ class Greeting:
         await message.reply(message_text_start, reply_markup=buttons, parse_mode=ParseMode.HTML)
 
     @dp.message_handler(commands=['help'])
-    async def send_help(message: types.Message) -> None:
+    async def send_help(message: Message) -> None:
         """
         This handler will be called when user sends `/help`
         :param message: types.Message
