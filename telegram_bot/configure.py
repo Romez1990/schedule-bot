3b129eabcce6b119108ec89d09cd7fe7b309dbca
Iglek Kuat <dizinnes228@gmail.com>
2020-08-31 10:41:17 +0600

    dev: add some configurations for the bot

--- telegram_bot/configure.py
+++ telegram_bot/configure.py
@@ -1,1 +1,15 @@
+from aiogram import Bot, Dispatcher
+
+import logging
+import os
+
+API_TOKEN = os.getenv('API_KEY_SCHEDULE_BOT')
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
+# Initialize bot and dispatcher
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
 
