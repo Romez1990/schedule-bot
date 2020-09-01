6b84ce6322769a6c0aa6b2063cc7b561b24bc977
Iglek Kuat <dizinnes228@gmail.com>
2020-09-01 10:59:41 +0600

    refactor: Changed import directory

--- src/telegram_bot/greeting.py
+++ src/telegram_bot/greeting.py
@@ -2,9 +2,9 @@ from aiogram import executor
 from aiogram.types import Message
 from aiogram.types import ParseMode
 
-from telegram_bot.configurations.configure import dp
-from telegram_bot.configurations.messages_text import message_text_start, message_text_help
-from telegram_bot.configurations.button_configuration import buttons
+from src.telegram_bot.configurations.configure import dp
+from .configurations.messages_text import message_text_start, message_text_help
+from .configurations.button_configuration import buttons
 
 import logging
 
@@ -13,11 +13,8 @@ logging.basicConfig(level=logging.INFO)
 
 
 class Greeting:
-    def __init__(self):
-        pass
-
     @dp.message_handler(commands=['start'])
-    async def send_welcome(message: Message) -> None:
+    async def send_welcome(self, message: Message) -> None:
         """
         This handler will be called when user sends `/start`
         :param message: types.Message
@@ -28,7 +25,7 @@ class Greeting:
         await message.reply(message_text_start, reply_markup=buttons, parse_mode=ParseMode.HTML)
 
     @dp.message_handler(commands=['help'])
-    async def send_help(message: Message) -> None:
+    async def send_help(self, message: Message) -> None:
         """
         This handler will be called when user sends `/help`
         :param message: types.Message
