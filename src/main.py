c4b2a3bcfdb9366551deb6624325ae3bf42c7e08
Iglek Kuat <dizinnes228@gmail.com>
2020-09-01 17:16:53 +0600

    fix: transfer of functionality from src in root directory

--- src/main.py
+++
@@ -1,9 +0,0 @@
-from .telegram_bot.telegram_bot import TelegramBot
-from .telegram_bot.subscription import Subscription
-from .telegram_bot.greeting import Greeting
-
-subscription = Subscription()
-greeting = Greeting()
-telegram_bot = TelegramBot(subscription, greeting)
-
-telegram_bot.start()
