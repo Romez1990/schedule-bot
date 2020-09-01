84f4d04cceb9ab5589c667666ad6d2ddffc50490
Iglek Kuat <dizinnes228@gmail.com>
2020-09-01 11:03:56 +0600

    refactor: global refactor by Romez1990

---
+++ src/telegram_bot/subscription.py
@@ -0,0 +1,19 @@
+from aiogram.types import ParseMode, Message
+
+import logging
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
+username = ''
+user_group = ''
+
+
+class Subscription:
+    async def subscribe(self, message: Message) -> None:
+        """
+        This handler will be called when user sends '/подписаться [Название_Группы]
+        :return: None
+        """
+        print(message.from_user.username)
+        print(message.text)
