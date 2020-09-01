0f9024dd84c251ed3df563f716538133f62ec9bc
Iglek Kuat <dizinnes228@gmail.com>
2020-09-01 18:26:48 +0600

    refactor: imports

--- src/telegram_bot/telegram_bot.py
+++ src/telegram_bot/telegram_bot.py
@@ -5,9 +5,8 @@ from .greeting import Greeting
 from .subscription import Subscription
 
 import logging
-import os
+from os import getenv
 
-API_TOKEN = os.getenv('API_KEY_SCHEDULE_BOT')
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
@@ -15,7 +14,7 @@ logging.basicConfig(level=logging.INFO)
 
 class TelegramBot:
     def __init__(self, subscription: Subscription, greeting: Greeting):
-        bot = Bot(token=API_TOKEN)
+        bot = Bot(token=getenv('API_KEY_SCHEDULE_BOT'))
         self.dispatcher = Dispatcher(bot)
         self.dispatcher.message_handler(commands=['подписаться'])(subscription.subscribe)
         self.dispatcher.message_handler(commands=['start'])(greeting.send_welcome)
@@ -23,3 +22,4 @@ class TelegramBot:
 
     def start(self):
         executor.start_polling(self.dispatcher, skip_updates=True)
+
