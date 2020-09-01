c260521c34c4c4da00d4a8bd621373aec0295a17
Iglek Kuat <dizinnes228@gmail.com>
2020-09-01 19:50:32 +0600

    refactor: delete unused import and make code sexy

--- src/telegram_bot/telegram_dispatcher.py
+++ src/telegram_bot/telegram_dispatcher.py
@@ -1,14 +1,13 @@
-from aiogram import Dispatcher
+from aiogram import Dispatcher, Bot
 from aiogram.utils import executor
 
 from .greeting import Greeting
 from .subscription import Subscription
-from .telegram_bot import TelegramBot
 
 
 class TelegramDispatcher:
-    def __init__(self, bot: TelegramBot, subscription: Subscription, greeting: Greeting):
-        self.dispatcher = Dispatcher(bot.bot)
+    def __init__(self, bot: Bot, subscription: Subscription, greeting: Greeting):
+        self.dispatcher = Dispatcher(bot)
         self.dispatcher.message_handler(commands=['подписаться'])(subscription.subscribe)
         self.dispatcher.message_handler(commands=['start'])(greeting.send_welcome)
         self.dispatcher.message_handler(commands=['help'])(greeting.send_help)
