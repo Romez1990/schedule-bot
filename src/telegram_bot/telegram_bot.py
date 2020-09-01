aa45eca65cb61214a6634f05947a638e39a0ad60
Iglek Kuat <dizinnes228@gmail.com>
2020-09-01 12:12:54 +0600

    fit: add new import and use them

--- src/telegram_bot/telegram_bot.py
+++ src/telegram_bot/telegram_bot.py
@@ -1,6 +1,9 @@
 from aiogram import Bot, Dispatcher
 from aiogram import executor
+
+from .greeting import Greeting
 from .subscription import Subscription
+
 import logging
 import os
 
@@ -14,10 +17,12 @@ logging.basicConfig(level=logging.INFO)
 
 
 class TelegramBot:
-    def __init__(self, subscription: Subscription):
+    def __init__(self, subscription: Subscription, greeting: Greeting):
         bot = Bot(token=API_TOKEN)
         self.dispatcher = Dispatcher(bot)
         self.dispatcher.message_handler(commands=['подписаться'])(subscription.subscribe)
+        self.dispatcher.message_handler(commands=['start'])(greeting.send_welcome)
+        self.dispatcher.message_handler(commands=['help'])(greeting.send_help)
 
     def start(self):
         executor.start_polling(self.dispatcher, skip_updates=True)
