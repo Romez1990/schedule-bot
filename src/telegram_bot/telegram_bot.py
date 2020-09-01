0758cff1bacb0db72153decc977939f416def453
Iglek Kuat <dizinnes228@gmail.com>
2020-09-01 19:49:49 +0600

    refactor: delete unused import

--- src/telegram_bot/telegram_bot.py
+++ src/telegram_bot/telegram_bot.py
@@ -1,15 +1,12 @@
 from aiogram import Bot
 
-from .greeting import Greeting
-from .subscription import Subscription
-
 from os import getenv
 
 
 class TelegramBot:
-    def __init__(self, subscription: Subscription, greeting: Greeting):
-        self._bot_api = Bot(token=getenv('API_KEY_SCHEDULE_BOT'))
+    def __init__(self):
+        self.__bot = Bot(token=getenv('API_KEY_SCHEDULE_BOT'))
 
     @property
-    def bot(self):
-        return self._bot_api
+    def bot(self) -> Bot:
+        return self.__bot
