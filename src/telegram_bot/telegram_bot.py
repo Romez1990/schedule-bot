481053f7317a6e47bca9252263ff5321cdeddd3a
Iglek Kuat <dizinnes228@gmail.com>
2020-09-01 19:05:05 +0600

    refactor: transfer dispatcher in myself class telegram_dispatcher.py

--- src/telegram_bot/telegram_bot.py
+++ src/telegram_bot/telegram_bot.py
@@ -1,5 +1,4 @@
-from aiogram import Bot, Dispatcher
-from aiogram import executor
+from aiogram import Bot
 
 from .greeting import Greeting
 from .subscription import Subscription
@@ -9,12 +8,8 @@ from os import getenv
 
 class TelegramBot:
     def __init__(self, subscription: Subscription, greeting: Greeting):
-        bot = Bot(token=getenv('API_KEY_SCHEDULE_BOT'))
-        self.dispatcher = Dispatcher(bot)
-        self.dispatcher.message_handler(commands=['подписаться'])(subscription.subscribe)
-        self.dispatcher.message_handler(commands=['start'])(greeting.send_welcome)
-        self.dispatcher.message_handler(commands=['help'])(greeting.send_help)
+        self._bot_api = Bot(token=getenv('API_KEY_SCHEDULE_BOT'))
 
-    def start(self):
-        executor.start_polling(self.dispatcher, skip_updates=True)
-
+    @property
+    def bot(self):
+        return self._bot_api
--- src/telegram_bot/telegram_dispatcher.py
+++ src/telegram_bot/telegram_dispatcher.py
@@ -1,15 +1,14 @@
 from aiogram import Dispatcher
 
-from .greeting import Greeting
-from .subscription import Subscription
 
 from .greeting import Greeting
 from .subscription import Subscription
+from .telegram_bot import TelegramBot
 
 
 class TelegramDispatcher:
-    def __init__(self, subscription: Subscription, greeting: Greeting):
-        self.dispatcher = Dispatcher()
+    def __init__(self, bot: TelegramBot,subscription: Subscription, greeting: Greeting):
+        self.dispatcher = Dispatcher(bot.bot)
         self.dispatcher.message_handler(commands=['подписаться'])(subscription.subscribe)
         self.dispatcher.message_handler(commands=['start'])(greeting.send_welcome)
         self.dispatcher.message_handler(commands=['help'])(greeting.send_help)
