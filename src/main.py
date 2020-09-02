671b43e9e0a0788564edee1591c069c565c5b291
Iglek Kuat <dizinnes228@gmail.com>
2020-09-02 19:34:39 +0600

    fit: add required dependencies, add file imports

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ from .telegram_bot.telegram_bot import TelegramBot
 from .telegram_bot.subscription import Subscription
 from .telegram_bot.greeting import Greeting
 from .telegram_bot.unsubscribe import Unsubscribe
+from .telegram_bot.theme_decoration import ThemeDecoration
 
 from .telegram_bot.telegram import Telegram
 from .telegram_bot.telegram_dispatcher import TelegramDispatcher
@@ -16,7 +17,8 @@ def main() -> None:
     subscription = Subscription(telegram_bot.bot)
     greeting = Greeting()
     unsubscribe = Unsubscribe(telegram_bot.bot)
+    theme_decoration = ThemeDecoration(telegram_bot.bot)
 
-    telegram_dispatcher = TelegramDispatcher(telegram_bot.bot, subscription, greeting, unsubscribe)
+    telegram_dispatcher = TelegramDispatcher(telegram_bot.bot, subscription, greeting, unsubscribe, theme_decoration)
     telegram = Telegram(telegram_dispatcher)
     telegram.start()
