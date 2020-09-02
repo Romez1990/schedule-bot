6f627b356d3c6da10f7c32780d314df268d74794
Iglek Kuat <dizinnes228@gmail.com>
2020-09-02 13:01:06 +0600

    refactor | fit: change method as async, and add new variables

--- src/telegram_bot/unsubscribe.py
+++ src/telegram_bot/unsubscribe.py
@@ -6,10 +6,14 @@ class Unsubscribe:
     def __init__(self, bot: Bot):
         self.bot = bot
 
-    def unsubscribe(self, message: Message) -> None:
+    async def unsubscribe(self, message: Message) -> None:
         """
         This method will be called when user write `/отписаться [Название_группы
         :param message:
         :return: None
         """
-        pass
+        username = message.from_user.username
+        group_input_from_user = message.text
+
+        user_group = group_input_from_user.split()
+
