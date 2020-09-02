a882c2ed781d7e70ec947ff36c203fe0099342c2
Iglek Kuat <dizinnes228@gmail.com>
2020-09-02 23:26:59 +0600

    refactor: delete file unsubscribe.py

--- src/main.py
+++ src/main.py
@@ -1,7 +1,6 @@
 from .telegram_bot.telegram_bot import TelegramBot
 from .telegram_bot.subscription import Subscription
 from .telegram_bot.greeting import Greeting
-from .telegram_bot.unsubscribe import Unsubscribe
 from .telegram_bot.theme_decoration import ThemeDecoration
 
 from .telegram_bot.telegram import Telegram
@@ -16,9 +15,8 @@ def main() -> None:
     telegram_bot = TelegramBot()
     subscription = Subscription(telegram_bot.bot)
     greeting = Greeting()
-    unsubscribe = Unsubscribe(telegram_bot.bot)
     theme_decoration = ThemeDecoration(telegram_bot.bot)
 
-    telegram_dispatcher = TelegramDispatcher(telegram_bot.bot, subscription, greeting, unsubscribe, theme_decoration)
+    telegram_dispatcher = TelegramDispatcher(telegram_bot.bot, subscription, greeting, theme_decoration)
     telegram = Telegram(telegram_dispatcher)
     telegram.start()
--- src/telegram_bot/telegram_dispatcher.py
+++ src/telegram_bot/telegram_dispatcher.py
@@ -3,18 +3,16 @@ from aiogram.utils import executor
 
 from .greeting import Greeting
 from .subscription import Subscription
-from .unsubscribe import Unsubscribe
 from .theme_decoration import ThemeDecoration
 
 
 class TelegramDispatcher:
-    def __init__(self, bot: Bot, subscription: Subscription, greeting: Greeting, unsubscribe: Unsubscribe,
+    def __init__(self, bot: Bot, subscription: Subscription, greeting: Greeting,
                  theme_decoration: ThemeDecoration):
         self.dispatcher = Dispatcher(bot)
         self.dispatcher.message_handler(commands=['подписаться'])(subscription.subscribe)
         self.dispatcher.message_handler(commands=['start'])(greeting.send_welcome)
         self.dispatcher.message_handler(commands=['help'])(greeting.send_help)
-        self.dispatcher.message_handler(commands=['отписаться'])(unsubscribe.unsubscribe)
         self.dispatcher.message_handler(commands=['тема'])(theme_decoration.ask_theme)
 
     def start(self) -> None:
