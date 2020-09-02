106f3536f40c9e20be23bbb7577cadc2a847a0c3
Iglek Kuat <dizinnes228@gmail.com>
2020-09-02 19:28:47 +0600

    fit: add class, function for sending data to the database, and function for input from user him theme decoration

---
+++ src/telegram_bot/theme_decoration.py
@@ -0,0 +1,28 @@
+from aiogram import Bot
+from aiogram.types import Message
+
+
+class ThemeDecoration:
+    def __init__(self, bot: Bot):
+        self.bot = bot
+
+    async def theme(self, theme: bool, username: str) -> None:
+        """
+        We will call this function at the bottom, and it will send the necessary requests to the database.
+        :param theme:
+        :param username:
+        :return: None
+        """
+        pass
+
+    async def ask_theme(self, message: Message) -> None:
+        username = message.from_user.username
+        input_from_user = message.text
+        ask_theme_from_user = input_from_user.split()
+
+        if ask_theme_from_user[1] == 'тёмная' or 'темная':
+            print(f'The {username} chose a {ask_theme_from_user[1]} theme')
+        elif ask_theme_from_user[1] == 'светлая' or 'белая':
+            print(f'The {username} chose a {ask_theme_from_user[2]} theme')
+        else:
+            raise SyntaxError('Error')
