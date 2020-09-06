627db401aa0b2b1c24589670d30cadf02d557a25
Iglek Kuat <dizinnes228@gmail.com>
2020-09-06 22:49:24 +0600

    refactor: rewrote in OOP style

--- src/telegram_bot/database.py
+++ src/telegram_bot/database.py
@@ -9,26 +9,28 @@ DB_USER = getenv('DB_USER_SCHEDULE_BOT')
 DB_PASS = getenv('DB_PASS_SCHEDULE_BOT')
 
 
-async def connect_to_database() -> asyncpg.connection:
-    connect = await asyncpg.connect(host=DB_HOST, database=DB_NAME, user=DB_USER, password=DB_PASS)
+class Database:
+    def __init__(self):
+        pass
 
-    try:
-        if connect:
-            print('truee')
-            return connect
-        else:
-            print('none')
-            return None
-    except asyncpg as ex:
-        print('error')
-        return f"Случилась ошибка, {ex}"
+    async def connect_to_database(self) -> asyncpg.connection:
+        connect = await asyncpg.connect(host=DB_HOST, database=DB_NAME, user=DB_USER, password=DB_PASS)
 
+        try:
+            if connect:
+                print('truee')
+                return connect
+            else:
+                print('none')
+                return None
+        except asyncpg as ex:
+            print('error')
+            return f"Случилась ошибка, {ex}"
 
-async def add_data_to_database(username: str, subscribe: bool) -> None:
-    connect = await connect_to_database()
-    await connect.execute('''
-        INSERT INTO users(username, subscribe) VALUES($1, $2)  
-    ''', username, subscribe)
+    async def add_data_to_database(self, username: str, subscribe: bool) -> None:
+        connect = await self.connect_to_database()
+        await connect.execute('''
+            INSERT INTO users(username, subscribe) VALUES($1, $2)  
+        ''', username, subscribe)
 
-
-asyncio.get_event_loop().run_until_complete(add_data_to_database('Dizi', False))
+    asyncio.get_event_loop().run_until_complete(add_data_to_database('Dizi', False))
