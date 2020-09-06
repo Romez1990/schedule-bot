b6361c34dbf96fb7c9e9592ca962ee9a0ac2d658
Iglek Kuat <dizinnes228@gmail.com>
2020-09-06 22:24:00 +0600

    refactor: delete unused code, add connect to database

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python runner.py $PORT
+web: python runner.py
--- src/telegram_bot/database.py
+++ src/telegram_bot/database.py
@@ -1,22 +1,27 @@
-from psycopg2 import connect
 from os import getenv
 
-import psycopg2
+import asyncio
+import asyncpg
 
 DB_HOST = 'localhost'
 DB_NAME = 'schedule_bot_db'
 DB_USER = getenv('DB_USER_SCHEDULE_BOT')
 DB_PASS = getenv('DB_PASS_SCHEDULE_BOT')
 
-conn = connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST)
 
-cur = conn.cursor()
+async def connect_to_database() -> asyncpg.connection:
+    connect = await asyncpg.connect(host=DB_HOST, database=DB_NAME, user=DB_USER, password=DB_PASS)
 
-cur.execute("INSERT INTO users (id, username, subscribe) VALUES (%s, %s, %s)",
-            (1,'Kuat', True))
+    try:
+        if connect:
+            print('truee')
+            return connect
+        else:
+            print('none')
+            return None
+    except asyncpg as ex:
+        print('error')
+        return f"Случилась ошибка, {ex}"
 
-conn.commit()
 
-cur.close()
 
-conn.close()
