d026ad5554c0d7f6e4b9b0384226ec4091cdbff0
Iglek Kuat <dizinnes228@gmail.com>
2020-09-08 11:47:50 +0600

    refactor: add delete method

--- src/repositories/user_repository.py
+++ src/repositories/user_repository.py
@@ -7,9 +7,13 @@ class UserRepository:
     def __init__(self, database: Database):
         self.database = database
 
-    async def add(self, platform: str, platform_id: str):
+    async def add(self, platform: str, platform_id: str) -> None:
         await self.database.execute('''
             INSERT INTO users(platform, platform_id) VALUES ($1, $2)
         ''', platform, platform_id)
 
-
+    async def delete(self, platform: str, platform_id: str) -> None:
+        await self.database.execute('''
+            DELETE from users
+            WHERE platform = ($1) and platform_id = ($2)
+        ''', platform, platform_id)
