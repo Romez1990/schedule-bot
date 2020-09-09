d1c5c447423b848634b517d27dae9ae6a2dd4f50
Iglek Kuat <dizinnes228@gmail.com>
2020-09-09 21:10:31 +0600

    fit: add database config, and update database.py

--- database.py
+++ database.py
@@ -5,17 +5,19 @@ from src.repositories import UserRepository
 from src.repositories import UserSettings
 from src.repositories import UserSubscribe
 
+from src.database.config import database_config as dc
+
 
 async def main():
-    database = Database()
+    database = Database(db_host=dc.DB_HOST, db_name=dc.DB_NAME, db_pass=dc.DB_PASS, db_user=dc.DB_USER)
     await database.connect()
     user_repository = UserRepository(database)
     user_settings = UserSettings(database)
     user_subscriptions = UserSubscribe(database)
-    # await user_repository.add('telegram', '228')
+    await user_repository.add('VK', '22822222')
     # await user_repository.delete('telegram', '228')
     # await user_settings.add(1, 'Тёмная')
-    await user_subscriptions.add(1, 'ЗВТ-18-9')
+    # await user_subscriptions.add(1, 'ЗВТ-18-9222')
 
 
 run(main())
---
+++ src/database/config/database_config.py
@@ -0,0 +1,6 @@
+from os import getenv
+
+DB_HOST = 'localhost'
+DB_NAME = 'schedule_bot_db'
+DB_USER = getenv('DB_USER_SCHEDULE_BOT')
+DB_PASS = getenv('DB_PASS_SCHEDULE_BOT')
