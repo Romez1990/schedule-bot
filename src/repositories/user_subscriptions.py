9900c9b53bd4d5453dd736b4938b7048f3e7091f
Iglek Kuat <dizinnes228@gmail.com>
2020-09-12 02:38:17 +0600

    fit: add comment for functions user_subscriptions.py

--- src/repositories/user_subscriptions.py
+++ src/repositories/user_subscriptions.py
@@ -6,6 +6,12 @@ class UserSubscribe:
         self.database = database
 
     async def add(self, user_id: int, group_name: str) -> None:
+        """
+        Here I call execute from database.py and add data for tables a `subscriptions`
+        :param user_id:
+        :param group_name:
+        :return:
+        """
         await self.database.execute('''
             INSERT INTO "subscriptions "(user_id, "group") VALUES ($1, $2)
         ''', user_id, group_name)
