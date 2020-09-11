eed65a2d2fd1a7b05b2f38001ff9d97efdf29f5e
Iglek Kuat <dizinnes228@gmail.com>
2020-09-12 02:30:21 +0600

    fit: add comment for functions

--- src/database/add_data.py
+++ src/database/add_data.py
@@ -13,16 +13,34 @@ class AddingData:
         self.database = Database(db_host=dc.DB_HOST, db_name=dc.DB_NAME, db_pass=dc.DB_PASS, db_user=dc.DB_USER)
 
     async def users(self, platform: str, platform_id: str) -> None:
+        """
+        Connect to table  users and add dates
+        :param platform:
+        :param platform_id:
+        :return: None
+        """
         await self.database.connect()
         user_repository = UserRepository(self.database)
         await user_repository.add(platform, platform_id)
 
     async def subscriptions(self, user_id: int, group_name: str) -> None:
+        """
+        Connect to table subscriptions and add dates
+        :param user_id:
+        :param group_name:
+        :return: None
+        """
         await self.database.connect()
         user_subscriptions = UserSubscribe(self.database)
         await user_subscriptions.add(user_id, group_name)
 
     async def user_settings(self, id: int, theme: str) -> None:
+        """
+        Connect to table user_settings and add dates
+        :param id:
+        :param theme:
+        :return:
+        """
         await self.database.connect()
         user_setting = UserSettings(self.database)
         await user_setting.add(id, theme)
