d5c4f67eceb30d48faaf0975ac78dfcc451674b2
Iglek Kuat <dizinnes228@gmail.com>
2020-09-11 23:19:01 +0600

    refactor: add main function and it's work

--- database.py
+++ database.py
@@ -4,33 +4,42 @@ from src.database import Database
 from src.repositories import UserRepository
 from src.repositories import UserSettings
 from src.repositories import UserSubscribe
+from src.database.add_data import AddingData
 
 from src.database.config import database_config as dc
 
 
-class AddingData:
-    def __init__(self):
-        self.database = Database(db_host=dc.DB_HOST, db_name=dc.DB_NAME, db_pass=dc.DB_PASS, db_user=dc.DB_USER)
+async def main():
+    add_data = AddingData()
+    await add_data.users('telegram', '123123124123')
+    await add_data.user_settings(6, 'Светлая')
+    await add_data.subscriptions(6, 'ЗВТ-19-8й123123')
 
-    async def users(self, platform: str, platform_id: str) -> None:
-        await self.database.connect()
-        user_repository = UserRepository(self.database)
-        await user_repository.add(platform, platform_id)
-
-    async def subscriptions(self, user_id: int, group_name: str) -> None:
-        await self.database.connect()
-        user_subscriptions = UserSubscribe(self.database)
-        await user_subscriptions.add(user_id, group_name)
-
-    async def user_settings(self, id: int, theme: str) -> None:
-        await self.database.connect()
-        user_setting = UserSettings(self.database)
-        await user_setting.add(id, theme)
-
-
-add_data = AddingData()
-run(add_data.user_settings(5, 'Тёмная'))
-
+run(main())
+# ---------------------------------------------------------------------------------------------------------------------
+# class AddingData:
+#     def __init__(self):
+#         self.database = Database(db_host=dc.DB_HOST, db_name=dc.DB_NAME, db_pass=dc.DB_PASS, db_user=dc.DB_USER)
+#
+#     async def users(self, platform: str, platform_id: str) -> None:
+#         await self.database.connect()
+#         user_repository = UserRepository(self.database)
+#         await user_repository.add(platform, platform_id)
+#
+#     async def subscriptions(self, user_id: int, group_name: str) -> None:
+#         await self.database.connect()
+#         user_subscriptions = UserSubscribe(self.database)
+#         await user_subscriptions.add(user_id, group_name)
+#
+#     async def user_settings(self, id: int, theme: str) -> None:
+#         await self.database.connect()
+#         user_setting = UserSettings(self.database)
+#         await user_setting.add(id, theme)
+#
+#
+# add_data = AddingData()
+# run(add_data.user_settings(5, 'Тёмная'))
+# ---------------------------------------------------------------------------------------------------------------------
 
 # ---------------------------------------------------------------------------------------------------------------------
 # async def users():
