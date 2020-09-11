c50237b011389ac1f1455398a35be889d47cd39d
Iglek Kuat <dizinnes228@gmail.com>
2020-09-12 02:37:34 +0600

    fit: add comment for functions user_settings.py

--- src/repositories/user_settings.py
+++ src/repositories/user_settings.py
@@ -6,6 +6,12 @@ class UserSettings:
         self.database = database
 
     async def add(self, id: int, theme: str):
+        """
+        Here I call execute from database.py and add data for tables a `user_settings`
+        :param id:
+        :param theme:
+        :return:
+        """
         await self.database.execute('''
             INSERT INTO user_settings(user_id, theme) VALUES ($1, $2)
         ''', id, theme)
