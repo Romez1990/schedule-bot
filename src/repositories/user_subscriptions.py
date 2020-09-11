14ebf7e9dfa0f96a930774223c915785f952e93f
Iglek Kuat <dizinnes228@gmail.com>
2020-09-12 02:48:47 +0600

    fit: add a remove function to remove data from databases user_subscription.py

--- src/repositories/user_subscriptions.py
+++ src/repositories/user_subscriptions.py
@@ -15,3 +15,15 @@ class UserSubscribe:
         await self.database.execute('''
             INSERT INTO "subscriptions "(user_id, "group") VALUES ($1, $2)
         ''', user_id, group_name)
+
+    async def delete(self, user_id: int, group_name: str) -> None:
+        """
+        Here I cal execute from database.py and delete dates from a tables `subscriptions`
+        :param user_id:
+        :param group_name:
+        :return:
+        """
+        await self.database.execute('''
+            DELETE from "subscriptions "
+            WHERE "user_id" = ($1) and "group" = ($1)
+        ''', user_id, group_name)
