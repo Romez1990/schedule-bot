291a7d6e592b40a31bd4eb6d960c95bda6cdd34b
Iglek Kuat <dizinnes228@gmail.com>
2020-09-12 02:48:58 +0600

    fit: add a remove function to remove data from databases user_settings.py

--- src/repositories/user_settings.py
+++ src/repositories/user_settings.py
@@ -5,13 +5,25 @@ class UserSettings:
     def __init__(self, database: Database):
         self.database = database
 
-    async def add(self, id: int, theme: str):
+    async def add(self, user_id: int, theme: str):
         """
         Here I call execute from database.py and add data for tables a `user_settings`
-        :param id:
+        :param user_id:
         :param theme:
         :return:
         """
         await self.database.execute('''
             INSERT INTO user_settings(user_id, theme) VALUES ($1, $2)
-        ''', id, theme)
+        ''', user_id, theme)
+
+    async def delete(self, user_id: int, theme: str) -> None:
+        """
+        Here I cal execute from database.py and delete dates from a tables `user_settings`
+        :param theme:
+        :param user_id:
+        :return:
+        """
+        await self.database.execute('''
+            DELETE from "user_settings"
+            WHERE "user_id" = ($1) and "theme" = ($1)
+        ''', user_id, theme)
