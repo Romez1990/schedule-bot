890b917747ee28f74740b0a8f77945bb8c09d361
Iglek Kuat <dizinnes228@gmail.com>
2020-09-12 02:36:41 +0600

    fit: add comment for functions user_repository.py

--- src/repositories/user_repository.py
+++ src/repositories/user_repository.py
@@ -6,11 +6,23 @@ class UserRepository:
         self.database = database
 
     async def add(self, platform: str, platform_id: str) -> None:
+        """
+        Here I call execute from database.py and add data for tables a `users`
+        :param platform:
+        :param platform_id:
+        :return:
+        """
         await self.database.execute('''
             INSERT INTO users(platform, platform_id) VALUES ($1, $2)
         ''', platform, platform_id)
 
     async def delete(self, platform: str, platform_id: str) -> None:
+        """
+        Here I call execute from database.py and delete data for tables a `users`
+        :param platform:
+        :param platform_id:
+        :return:
+        """
         await self.database.execute('''
             DELETE from users
             WHERE platform = ($1) and platform_id = ($2)
