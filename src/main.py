32abe0bac654f591346b03a634fe2e1b04b42bcc
Iglek Kuat <dizinnes228@gmail.com>
2020-09-12 22:04:41 +0600

    refactor: update main function, and add new function for run bot

--- src/main.py
+++ src/main.py
@@ -1,3 +1,8 @@
+from asyncio import get_event_loop
+
+from .database import Database
+from .repositories import UserRepository
+from .services.user_service import UserService
 from .telegram_bot.telegram_bot import TelegramBot
 from .telegram_bot.subscription import Subscription
 from .telegram_bot.greeting import Greeting
@@ -7,16 +12,26 @@ from .telegram_bot.telegram import Telegram
 from .telegram_bot.telegram_dispatcher import TelegramDispatcher
 
 
-def main() -> None:
+async def async_main() -> None:
     """
     This function for init then we transfer it to main.py in root directory
     :return: None
     """
     telegram_bot = TelegramBot()
+    database = Database()
+    await database.connect()
+    user_repository = UserRepository(database)
+    user_service = UserService(user_repository)
     subscription = Subscription(telegram_bot.bot)
-    greeting = Greeting(telegram_bot.bot)
+    greeting = Greeting(telegram_bot.bot, user_service)
     theme_decoration = ThemeDecoration(telegram_bot.bot)
 
     telegram_dispatcher = TelegramDispatcher(telegram_bot.bot, subscription, greeting, theme_decoration)
     telegram = Telegram(telegram_dispatcher)
     telegram.start()
+
+
+def main():
+    loop = get_event_loop()
+    loop.create_task(async_main())
+    loop.run_forever()
