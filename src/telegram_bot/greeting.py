a2a3242d0c462fbc7d11cc5f76360f11a8d2228b
Iglek Kuat <dizinnes228@gmail.com>
2020-09-12 22:03:37 +0600

    refactor: change imports and add data to database

--- src/telegram_bot/greeting.py
+++ src/telegram_bot/greeting.py
@@ -4,11 +4,13 @@ from aiogram.types import ParseMode
 
 from .configurations.messages_text import message_text_start, message_text_help
 from .configurations.button_configuration import buttons
+from ..services.user_service import UserService
 
 
 class Greeting:
-    def __init__(self, bot: Bot):
+    def __init__(self, bot: Bot, user_service: UserService):
         self.bot = bot
+        self.user_service = user_service
 
     async def send_welcome(self, message: Message) -> None:
         """
@@ -18,6 +20,7 @@ class Greeting:
         """
         # message_text_start are taken from directory configurations/messages_text.py
         # buttons are taken from directory configurations/buttons.py
+        await self.user_service.add('Telegram', str(message.from_user.id))
         await self.bot.send_message(message.from_user.id, message_text_start(), reply_markup=buttons,
                                     parse_mode=ParseMode.HTML)
 
