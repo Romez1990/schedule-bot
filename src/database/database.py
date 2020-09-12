799772560317562da4d2e1f39fec57f66c9ebb87
Iglek Kuat <dizinnes228@gmail.com>
2020-09-12 22:03:04 +0600

    refactor: delete global variables

--- src/database/database.py
+++ src/database/database.py
@@ -4,18 +4,13 @@ from typing import Any
 
 import asyncpg
 
-HOST = 'localhost'
-NAME = 'schedule_bot_db'
-USER = getenv('USER_SCHEDULE_BOT')
-PASS = getenv('PASS_SCHEDULE_BOT')
-
 
 class Database:
-    def __init__(self, host, name, user, password):
-        self.host = host
-        self.name = name
-        self.user = user
-        self.password = password
+    def __init__(self):
+        self.host = getenv('DB_HOST')
+        self.name = getenv('DB_NAME')
+        self.user = getenv('DB_USER')
+        self.password = getenv('DB_PASSWORD')
 
     connection: asyncpg.connection.Connection
 
