5e58daf11a75e248513f36eac8c66b89fe9a3ba0
Romez <Romez1990@gmail.com>
2020-09-12 14:49:47 +0300

    feat(schedule): create group name parsing error

--- src/schedule/__init__.py
+++ src/schedule/__init__.py
@@ -2,6 +2,7 @@ from .schedule import Schedule
 from .group import Group
 from .university_group import UniversityGroup
 from .college_group import CollegeGroup
+from .errors import GroupNameParsingException
 from .group_schedule import GroupSchedule
 from .week_day import WeekDay
 from .day_schedule import DaySchedule
