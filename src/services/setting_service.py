c4d3f4ecde94b0b5e24f86cdc30ad5c863df4b95
Iglek Kuat <dizinnes228@gmail.com>
2020-09-13 02:19:04 +0600

    fit: add class and method for add dates

---
+++ src/services/setting_service.py
@@ -0,0 +1,9 @@
+from ..repositories import UserSettings
+
+
+class SettingService:
+    def __init__(self, user_settings: UserSettings):
+        self.user_settings = user_settings
+
+    async def add(self, user_id, theme):
+        await self.user_settings.add(user_id, theme)
