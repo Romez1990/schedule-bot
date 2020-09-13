b225af1dbe4ce0a6c3cc79c3bd09edafc3f96071
Iglek Kuat <dizinnes228@gmail.com>
2020-09-13 13:51:03 +0600

    refactor: add typing for setting_service.py and subscription_service.py

--- src/services/setting_service.py
+++ src/services/setting_service.py
@@ -5,8 +5,8 @@ class SettingService:
     def __init__(self, user_settings: UserSettings):
         self.user_settings = user_settings
 
-    async def add(self, user_id, theme):
+    async def add(self, user_id: int, theme: str) -> None:
         await self.user_settings.add(user_id, theme)
 
-    async def delete(self, user_id, theme):
+    async def delete(self, user_id: int, theme: str) -> None:
         await self.user_settings.delete(user_id, theme)
--- src/services/subscription_service.py
+++ src/services/subscription_service.py
@@ -5,8 +5,8 @@ class SubscriptionService:
     def __init__(self, user_subscription: UserSubscribe):
         self.user_subscription = user_subscription
 
-    async def add(self, user_id, group_name):
+    async def add(self, user_id: int, group_name: str) -> None:
         await self.user_subscription.add(user_id, group_name)
 
-    async def delete(self, user_id, group_name):
+    async def delete(self, user_id: int, group_name: str) -> None:
         await self.user_subscription.delete(user_id, group_name)
