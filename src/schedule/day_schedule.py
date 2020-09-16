a91f20248f6fcace37cbabafc2e6dacc17d13ff7
Romez <Romez1990@gmail.com>
2020-09-16 16:49:05 +0300

    feat(schedule): make day schedule sized

--- src/schedule/day_schedule.py
+++ src/schedule/day_schedule.py
@@ -2,6 +2,7 @@ from typing import (
     Iterable,
     Iterator,
     Reversible,
+    Sized,
 )
 from pyrsistent import pvector
 from returns.maybe import Maybe, Nothing
@@ -9,12 +10,15 @@ from returns.maybe import Maybe, Nothing
 from .entry import Entry
 
 
-class DaySchedule(Reversible[Maybe[Entry]]):
+class DaySchedule(Reversible[Maybe[Entry]], Sized):
     def __init__(self, entries: Iterable[Maybe[Entry]]) -> None:
         self.__entries = pvector(entries)
 
     def __iter__(self) -> Iterator[Maybe[Entry]]:
         return iter(self.__entries)
+
+    def __len__(self) -> int:
+        return len(self.__entries)
 
     def __reversed__(self) -> Iterator[Maybe[Entry]]:
         return iter(reversed(self.__entries))
