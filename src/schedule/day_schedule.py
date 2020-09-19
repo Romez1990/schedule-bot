709770aab6bf19516fa66aeb3715516d70db796c
Romez <Romez1990@gmail.com>
2020-09-19 08:03:05 +0300

    feat(schedule): make day schedule reversible

--- src/schedule/day_schedule.py
+++ src/schedule/day_schedule.py
@@ -1,6 +1,7 @@
 from typing import (
     Iterable,
     Iterator,
+    Reversible,
 )
 from pyrsistent import pvector
 from returns.maybe import Maybe
@@ -8,9 +9,12 @@ from returns.maybe import Maybe
 from .entry import Entry
 
 
-class DaySchedule(Iterable[Maybe[Entry]]):
+class DaySchedule(Reversible[Maybe[Entry]]):
     def __init__(self, entries: Iterable[Maybe[Entry]]) -> None:
         self.__entries = pvector(entries)
 
     def __iter__(self) -> Iterator[Maybe[Entry]]:
         return iter(self.__entries)
+
+    def __reversed__(self) -> Iterator[Maybe[Entry]]:
+        return iter(reversed(self.__entries))
