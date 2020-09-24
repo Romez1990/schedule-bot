28aad630af856efe5d64a0afbe27544789e907f9
Romez <Romez1990@gmail.com>
2020-09-24 21:04:05 +0300

    feat(telegram_service): add send message method to telegram bot

--- src/telegram_service/telegram_bot.py
+++ src/telegram_service/telegram_bot.py
@@ -1,9 +1,16 @@
 from typing import (
     Callable,
+    Union,
     TypeVar,
 )
-
 from aiogram import Bot
+from aiogram.types import (
+    Message,
+    InlineKeyboardMarkup,
+    ReplyKeyboardMarkup,
+    ReplyKeyboardRemove,
+    ForceReply,
+)
 
 from ..env import AbstractEnvironment
 
@@ -17,3 +24,11 @@ class TelegramBot:
 
     def register_bot(self, func: Callable[[Bot], T]) -> T:
         return func(self.__bot)
+
+    async def send_message(self, chat_id: str, text: str, *, parse_mode: str = None,
+                           disable_web_page_preview: bool = None, disable_notification: bool = None,
+                           reply_to_message_id: int = None,
+                           reply_markup: Union[InlineKeyboardMarkup, ReplyKeyboardMarkup, ReplyKeyboardRemove,
+                                               ForceReply] = None) -> Message:
+        return await self.__bot.send_message(chat_id, text, parse_mode, disable_web_page_preview, disable_notification,
+                                             reply_to_message_id, reply_markup)
