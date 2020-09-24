cb80e3f18a98e0461a4a38e472158223ec54c535
Romez <Romez1990@gmail.com>
2020-09-24 20:21:17 +0300

    refactor(telegram_service): make fields private in dispatcher

--- src/telegram_service/telegram_dispatcher.py
+++ src/telegram_service/telegram_dispatcher.py
@@ -11,13 +11,13 @@ from .theme_decoration import ThemeDecoration
 class TelegramDispatcher(AbstractTelegramDispatcher):
     def __init__(self, bot: Bot, subscription: Subscription, greeting: Greeting,
                  theme_decoration: ThemeDecoration):
-        self.dispatcher = Dispatcher(bot)
-        self.dispatcher.message_handler(commands=['подписаться'])(subscription.subscribe)
-        self.dispatcher.message_handler(commands=['отписаться'])(subscription.unsubscribe)
-        self.dispatcher.message_handler(commands=['изменить'])(subscription.change)
-        self.dispatcher.message_handler(commands=['start'])(greeting.send_welcome)
-        self.dispatcher.message_handler(commands=['help'])(greeting.send_help)
-        self.dispatcher.message_handler(commands=['тема'])(theme_decoration.ask_theme)
+        self.__dispatcher = Dispatcher(bot)
+        self.__dispatcher.message_handler(commands=['подписаться'])(subscription.subscribe)
+        self.__dispatcher.message_handler(commands=['отписаться'])(subscription.unsubscribe)
+        self.__dispatcher.message_handler(commands=['изменить'])(subscription.change)
+        self.__dispatcher.message_handler(commands=['start'])(greeting.send_welcome)
+        self.__dispatcher.message_handler(commands=['help'])(greeting.send_help)
+        self.__dispatcher.message_handler(commands=['тема'])(theme_decoration.ask_theme)
 
     def start(self) -> None:
         """
@@ -25,5 +25,5 @@ class TelegramDispatcher(AbstractTelegramDispatcher):
         :return: None
         """
         loop = get_event_loop()
-        loop.create_task(self.dispatcher.start_polling())
+        loop.create_task(self.__dispatcher.start_polling())
         print('Telegram bot has been started')
