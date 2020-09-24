51dd7dfc8ce460ad2eb600eaaa8a0d34f526e13b
Romez <Romez1990@gmail.com>
2020-09-24 20:30:54 +0300

    feat(telegram_service): create method for registering bot in telegram bot

--- src/telegram_service/telegram_bot.py
+++ src/telegram_service/telegram_bot.py
@@ -1,9 +1,19 @@
+from typing import (
+    Callable,
+    TypeVar,
+)
+
 from aiogram import Bot
 
 from ..env import AbstractEnvironment
+
+T = TypeVar('T')
 
 
 class TelegramBot:
     def __init__(self, env: AbstractEnvironment):
         token = env.get_str('TELEGRAM_BOT_TOKEN')
         self.__bot = Bot(token=token)
+
+    def register_bot(self, func: Callable[[Bot], T]) -> T:
+        return func(self.__bot)
