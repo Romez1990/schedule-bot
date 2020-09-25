812f7378f3fc41e2a8810b73a5ac9ed9f666529d
Iglek Kuat <dizinnes228@gmail.com>
2020-09-25 23:42:10 +0600

    refactor(vk_service): rewrite code to OOP style

--- src/vk_service/greeting.py
+++ src/vk_service/greeting.py
@@ -1,17 +1,15 @@
 from vkwave.bots import SimpleLongPollUserBot
-
-bot = SimpleLongPollUserBot(
-    tokens="d3a83eb6b7c23bf02582bd23281b23702190692175bcdc36bd3171c39e53768c07e0a4a3c14ea28322ba6", )
+from vkwave.bots import SimpleBotEvent
 
 
-@bot.message_handler(bot.command_filter('start'))
-async def start(event: bot.SimpleBotEvent):
-    await event.answer('start')
+class Greeting:
+    def __init__(self, bot: SimpleLongPollUserBot):
+        self.bot = bot
 
+    async def start(self, event: SimpleBotEvent):
+        await event.answer('start')
 
-@bot.message_handler(bot.command_filter('help'))
-async def help(event: bot.SimpleBotEvent):
-    await event.answer('help')
+    async def help(self, event: SimpleBotEvent):
+        await event.answer('help')
 
-
-bot.run_forever()
+# bot.run_forever()
