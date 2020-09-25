b71b128b3c38f275342febec7bbb653b99b4b188
Iglek Kuat <dizinnes228@gmail.com>
2020-09-25 23:27:41 +0600

    refactor(vk_service): change await.answer text

--- src/vk_service/greeting.py
+++ src/vk_service/greeting.py
@@ -4,14 +4,14 @@ bot = SimpleLongPollUserBot(
     tokens="d3a83eb6b7c23bf02582bd23281b23702190692175bcdc36bd3171c39e53768c07e0a4a3c14ea28322ba6", )
 
 
-@bot.message_handler(bot.text_filter("bye"))
-def handle(_) -> str:
-    return "bye world"
+@bot.message_handler(bot.command_filter('start'))
+async def start(event: bot.SimpleBotEvent):
+    await event.answer('start')
 
 
-@bot.message_handler(bot.text_filter("hello"))
-async def handle(event: bot.SimpleBotEvent):
-    await event.answer("hello world!")
+@bot.message_handler(bot.command_filter('help'))
+async def help(event: bot.SimpleBotEvent):
+    await event.answer('help')
 
 
 bot.run_forever()
