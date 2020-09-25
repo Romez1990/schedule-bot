b2667b9f625b940f8c1619f1cb504048db457d19
Romez <Romez1990@gmail.com>
2020-09-25 15:28:08 +0300

    refactor(telegram_service): remove bot getter in telegram bot

--- src/telegram_service/telegram_bot.py
+++ src/telegram_service/telegram_bot.py
@@ -7,11 +7,3 @@ class TelegramBot:
     def __init__(self, env: AbstractEnvironment):
         token = env.get_str('TELEGRAM_BOT_TOKEN')
         self.__bot = Bot(token=token)
-
-    @property
-    def bot(self) -> Bot:
-        """
-        This property function return bot
-        :return: Bot
-        """
-        return self.__bot
