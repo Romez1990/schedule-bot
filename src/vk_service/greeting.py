ac972e6ffd039a4f2ad20f06678fcb78763c36ea
Iglek Kuat <dizinnes228@gmail.com>
2020-09-25 22:50:19 +0600

    fix(vk_service): replace SimpleLongPollBot with SimpleLongPollUserBot

--- src/vk_service/greeting.py
+++ src/vk_service/greeting.py
@@ -1,7 +1,7 @@
-from vkwave.bots import SimpleLongPollBot
+from vkwave.bots import SimpleLongPollUserBot
 
-bot = SimpleLongPollBot(tokens="d3a83eb6b7c23bf02582bd23281b23702190692175bcdc36bd3171c39e53768c07e0a4a3c14ea28322ba6",
-                        group_id=196098913)
+bot = SimpleLongPollUserBot(
+    tokens="d3a83eb6b7c23bf02582bd23281b23702190692175bcdc36bd3171c39e53768c07e0a4a3c14ea28322ba6", )
 
 
 @bot.message_handler(bot.text_filter("bye"))
