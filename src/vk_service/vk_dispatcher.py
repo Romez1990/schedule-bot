4353a52d1de6923382ebbaa74e7db1b207e255d4
Iglek Kuat <dizinnes228@gmail.com>
2020-09-26 21:03:02 +0600

    feat(vk_service): create vk_dispatcher.py in vk_service

--- src/vk_service/vk_dispatcher.py
+++ src/vk_service/vk_dispatcher.py
@@ -1,5 +1,3 @@
-from vkwave.bots import simple_user_message_handler
-
 from .vk_bot import VkBot
 from .greeting import Greeting
 from .subscription import Subscription
@@ -7,7 +5,7 @@ from .subscription import Subscription
 
 class VkDispatcher:
     def __init__(self, bot: VkBot, greeting: Greeting, subscription: Subscription):
-        self.bot = bot,
+        self.bot = VkBot,
         self.greeting = greeting,
         self.subscription = subscription
         self.dispatcher = simple_user_message_handler(router=bot)
