90222d9dcd9e5a5de6a4b9979ea75b8b01133597
Iglek Kuat <dizinnes228@gmail.com>
2020-09-27 12:26:31 +0600

    refactor(vk_service): add imports and add new functional

--- src/vk_service/vk_bot.py
+++ src/vk_service/vk_bot.py
@@ -1,10 +1,16 @@
 from vkwave.bots import SimpleLongPollUserBot
 
+from ..env import AbstractEnvironment
+from .abstract_vk_controller import AbstractVkController
+from .abstract_vk_bot import AbstractVkBot
 
-class VkBot:
-    def __init__(self):
-        self.__bot = SimpleLongPollUserBot(tokens="QWERTY")
 
-    @property
-    def bot(self) -> SimpleLongPollUserBot:
-        return self.__bot
+class VkBot(AbstractVkBot):
+    def __init__(self, env: AbstractEnvironment, vk_controller: AbstractVkController) -> None:
+        token = env.get_str('VK_BOT_TOKEN')
+        self.__bot = SimpleLongPollUserBot(tokens=token)
+        self.__bot.message_handler(self.__bot.command_filter('start'))(vk_controller.welcome)
+        self.__bot.message_handler(self.__bot.command_filter('help'))(vk_controller.help)
+
+    async def start(self) -> None:
+        await self.__bot.run_forever()
