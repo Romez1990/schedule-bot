7813c8a485e25cb0631904a01d2d0f3113d378a6
Romez <Romez1990@gmail.com>
2020-09-27 10:08:24 +0300

    feat(main): use ioc container

--- src/main.py
+++ src/main.py
@@ -1,31 +1,9 @@
 from asyncio import get_event_loop, AbstractEventLoop
 
-from src.env import (
-    BaseEnvironment,
-    Environment,
-)
-from src.schedule import (
-    GroupParser,
-    WeekDayTranslator,
-)
-from src.database import (
-    Database,
-)
-from src.repositories import (
-    UserRepository,
-    UserSettingsRepository,
-    SubscriptionRepository,
-)
-from src.bot_services import (
-    UserService,
-    UserSettingsService,
-    SubscriptionService,
-)
-from src.telegram_service import (
-    TelegramService,
-    TelegramBot,
-    TelegramController,
-)
+from src.app_container_builder import AppContainerBuilder
+from src.env import AbstractEnvironment
+from src.database import AbstractDatabase
+from src.telegram_service import TelegramService
 
 
 async def main(loop: AbstractEventLoop) -> None:
@@ -33,20 +11,12 @@ async def main(loop: AbstractEventLoop) -> None:
     This function for init then we transfer it to main.py in root directory
     :return: None
     """
-    base_env = BaseEnvironment()
-    env = Environment(base_env)
-    group_parser = GroupParser()
-    week_day_translator = WeekDayTranslator()
-    database = Database(env)
-    user_repository = UserRepository(database)
-    user_settings_repository = UserSettingsRepository(database)
-    subscription_repository = SubscriptionRepository(database)
-    user_settings_service = UserSettingsService(user_settings_repository)
-    subscription_service = SubscriptionService(subscription_repository, group_parser)
-    user_service = UserService(user_repository, user_settings_service, subscription_service)
-    telegram_bot = TelegramBot(env)
-    controller = TelegramController(telegram_bot, user_service, user_settings_service, subscription_service)
-    telegram_service = TelegramService(telegram_bot, controller)
+    container_builder = AppContainerBuilder()
+    container = container_builder.build()
+
+    env = container.get(AbstractEnvironment)
+    database = container.get(AbstractDatabase)
+    telegram_service = container.get(TelegramService)
 
     env.read()
     await database.connect()
