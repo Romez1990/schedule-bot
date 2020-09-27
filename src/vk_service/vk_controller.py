862f058eb89674e10bfd9ba60bdb3a5b2cb7579c
Iglek Kuat <dizinnes228@gmail.com>
2020-09-27 14:52:09 +0600

    refactor(vk_service): add constructor and add answer for methods

--- src/vk_service/vk_controller.py
+++ src/vk_service/vk_controller.py
@@ -1,9 +1,14 @@
 from vkwave.bots import SimpleBotEvent
 
+from .vk_bot import VkBot
+
 
 class VkController:
+    def __init__(self, bot: VkBot):
+        self.__bot = bot
+
     async def welcome(self, event: SimpleBotEvent) -> None:
-        await event.answer('')
+        await event.answer('welcome')
 
     async def help(self, event: SimpleBotEvent) -> None:
-        await event.answer('')
+        await event.answer('help')
