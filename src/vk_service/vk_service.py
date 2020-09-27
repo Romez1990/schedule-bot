7d881556b5aaf8d1a2bc9f2db8d2ff2b82b7d5d8
Iglek Kuat <dizinnes228@gmail.com>
2020-09-27 14:52:27 +0600

    refactor(vk_service): add constructor and add new imports

--- src/vk_service/vk_service.py
+++ src/vk_service/vk_service.py
@@ -1,9 +1,14 @@
+from .vk_bot import VkBot
+from .vk_controller import VkController
 from ..platform_service import PlatformService
 
 
 class VkService(PlatformService):
-    def __init__(self, dispatcher: None) -> None:  # Change dispatcher type change to another
-        self.__dispatcher = dispatcher
+    def __init__(self, bot: VkBot, controller: VkController) -> None:  # Change dispatcher type change to another
+        self.__bot = bot
+        self.__controller = controller
 
     async def start(self) -> None:
-        await self.__dispatcher.start_polling()
+        self.__bot.init()
+
+
