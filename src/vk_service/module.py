9a4307e3651a72851dd68a608d7636d810734714
Iglek Kuat <dizinnes228@gmail.com>
2020-09-27 15:12:02 +0600

    feat(vk_service): add class and method

--- src/vk_service/module.py
+++ src/vk_service/module.py
@@ -4,3 +4,8 @@ from .vk_bot import VkBot
 from .vk_controller import VkController
 
 
+class VkServiceModule(Module):
+    def _load(self, builder: ContainerBuilder):
+        builder.bind(VkService).to_self()
+        builder.bind(VkBot).to_self()
+        builder.bind(VkController).to_self()
