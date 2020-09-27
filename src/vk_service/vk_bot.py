ab7cd8b8252f902727d2013ad653955ad405f851
Iglek Kuat <dizinnes228@gmail.com>
2020-09-27 14:51:20 +0600

    refactor(vk_service): move bot initializing to init method

--- src/vk_service/vk_bot.py
+++ src/vk_service/vk_bot.py
@@ -1,3 +1,6 @@
+from typing import (
+    Optional,
+)
 from vkwave.bots import SimpleLongPollUserBot
 
 from ..env import AbstractEnvironment
@@ -6,10 +9,20 @@ from .vk_controller import VkController
 
 class VkBot:
     def __init__(self, env: AbstractEnvironment, vk_controller: VkController) -> None:
-        token = env.get_str('VK_BOT_TOKEN')
+        self.__env = env
+        self.__vk_controller = vk_controller
+        self.__bot: Optional[SimpleLongPollUserBot] = None
+
+    def init(self) -> None:
+        token = self.__env.get_str('VK_BOT_TOKEN')
         self.__bot = SimpleLongPollUserBot(tokens=token)
-        self.__bot.message_handler(self.__bot.command_filter('start'))(vk_controller.welcome)
-        self.__bot.message_handler(self.__bot.command_filter('help'))(vk_controller.help)
 
     async def start(self) -> None:
+        if self.__bot is None:
+            self.__raise_init_error()
+        self.__bot.message_handler(self.__bot.command_filter('start'))(self.__vk_controller.welcome)
+        self.__bot.message_handler(self.__bot.command_filter('help'))(self.__vk_controller.help)
         await self.__bot.run_forever()
+
+    def __raise_init_error(self) -> None:
+        raise Exception('Vk bot has not been inited')
