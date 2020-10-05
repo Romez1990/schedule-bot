476d66dc60f14f21fd7b1a93b00bdac56987709d
Romez <Romez1990@gmail.com>
2020-10-05 16:55:54 +0300

    feat(schedule_services): create renderer package

--- src/schedule_services/module.py
+++ src/schedule_services/module.py
@@ -1,9 +1,11 @@
 from src.ioc_container import Module, ContainerBuilder
 from .parser import ParserModule
 from .scraper import ScraperModule
+from .renderer import RendererModule
 
 
 class ScheduleServicesModule(Module):
     def _load(self, builder: ContainerBuilder) -> None:
         builder.register_module(ParserModule)
         builder.register_module(ScraperModule)
+        builder.register_module(RendererModule)
---
+++ src/schedule_services/renderer/module.py
@@ -0,0 +1,6 @@
+from src.ioc_container import Module, ContainerBuilder
+
+
+class RendererModule(Module):
+    def _load(self, builder: ContainerBuilder) -> None:
+        pass
