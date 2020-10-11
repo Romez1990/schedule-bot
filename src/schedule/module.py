8b424237a52a299b944a96199da6ba6df2726805
Romez <Romez1990@gmail.com>
2020-10-11 20:20:54 +0300

    refactor(schedule): update schedule to work with new container

--- src/schedule/module.py
+++ src/schedule/module.py
@@ -1,4 +1,4 @@
-from src.ioc_container import Module, ContainerBuilder
+from src.ioc_container import Module, Container
 from .group_parser_interface import GroupParserInterface
 from .group_parser import GroupParser
 from .day_of_week_translator_interface import DayOfWeekTranslatorInterface
@@ -6,6 +6,6 @@ from .day_of_week_translator import DayOfWeekTranslator
 
 
 class ScheduleModule(Module):
-    def _load(self, builder: ContainerBuilder) -> None:
-        builder.bind(GroupParser).to(GroupParserInterface)
-        builder.bind(DayOfWeekTranslator).to(DayOfWeekTranslatorInterface)
+    def _load(self, container: Container) -> None:
+        container.bind(GroupParser).to(GroupParserInterface)
+        container.bind(DayOfWeekTranslator).to(DayOfWeekTranslatorInterface)
