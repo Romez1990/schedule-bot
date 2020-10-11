a0eba5e5b8a218ccfb36ac942631a1eb025f979f
Iglek Kuat <dizinnes228@gmail.com>
2020-10-12 00:25:13 +0600

    refactor(bot): update imports and refactoring VkController

--- src/platform_services/vk_service/vk_controller.py
+++ src/platform_services/vk_service/vk_controller.py
@@ -1,27 +1,22 @@
 from vkwave.bots import SimpleBotEvent
 
+from ..button_configuration import ButtonConfiguration
+from ..messages_text import MessageText
 from .vk_bot import VkBot
-from ...general_settings.messages_text import (
-    message_text_start,
-    message_text_help,
-)
-
-from ...general_settings.button_configuration import (
-    ButtonConfiguration
-)
 
 
 class VkController:
-    def __init__(self, bot: VkBot, vk_button: ButtonConfiguration) -> None:
+    def __init__(self, bot: VkBot, vk_button: ButtonConfiguration, message_text: MessageText) -> None:
         self.__bot = bot
         self.__vk_button = vk_button
+        self.__message_text = message_text
 
     async def welcome(self, event: SimpleBotEvent) -> None:
         button = self.__vk_button.vk_buttons()
-        await event.answer(message=message_text_start(),
+        await event.answer(message=self.__message_text.message_text_start(),
                            keyboard=button.get_keyboard())
 
     async def help(self, event: SimpleBotEvent) -> None:
         button = self.__vk_button.vk_buttons()
-        await event.answer(message=message_text_help(),
+        await event.answer(message=self.__message_text.message_text_help(),
                            keyboard=button.get_keyboard())
