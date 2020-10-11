4427e9b75a745ba1ba04a1cb07ea215aee88d814
Iglek Kuat <dizinnes228@gmail.com>
2020-10-12 00:31:21 +0600

    feat(bot): update imports and add bind button configuration and message text

--- src/platform_services/module.py
+++ src/platform_services/module.py
@@ -1,9 +1,13 @@
 from src.ioc_container import Module, Container
 from .telegram_service import TelegramServiceModule
 from .vk_service import VkServiceModule
+from .button_configuration import ButtonConfiguration
+from .messages_text import MessageText
 
 
 class PlatformServicesModule(Module):
     def _load(self, container: Container) -> None:
         container.register_module(TelegramServiceModule)
         container.register_module(VkServiceModule)
+        container.bind(ButtonConfiguration).to_self()
+        container.bind(MessageText).to_self()
