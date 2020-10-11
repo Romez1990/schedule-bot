17c69684feac4db763d7f339b80ca2bf2b3f1972
Iglek Kuat <dizinnes228@gmail.com>
2020-10-11 15:24:01 +0600

    refactor(telegram_service): add telegram buttons to telegram_controller.py from methods

--- src/platform_services/telegram_service/telegram_controller.py
+++ src/platform_services/telegram_service/telegram_controller.py
@@ -11,6 +11,7 @@ from src.bot_services import (
 from .telegram_bot import TelegramBot
 from src.general_settings.messages_text import message_text_start, message_text_help
 from src.general_settings.button_configuration import buttons
+from src.general_settings.button_configuration import ButtonConfiguration
 
 
 class TelegramController:
@@ -20,23 +21,27 @@ class TelegramController:
             user_service: UserServiceInterface,
             user_settings_service: UserSettingsServiceInterface,
             subscription_service: SubscriptionServiceInterface,
+            button_configuration: ButtonConfiguration,
     ) -> None:
         self.__bot = bot
         self.__user_service = user_service
         self.__user_settings_service = user_settings_service
         self.__subscription_service = subscription_service
         self.__platform = 'telegram'
+        self.button_configuration = button_configuration
 
     async def welcome(self, message: Message) -> None:
         telegram_id = self.__get_telegram_id(message)
         await self.__user_service.create_if_not_exists(self.__platform, telegram_id)
         await self.__bot.send_message(telegram_id, message_text_start(),
-                                      reply_markup=buttons, parse_mode=ParseMode.HTML)
+                                      reply_markup=self.button_configuration.telegram_buttons(),
+                                      parse_mode=ParseMode.HTML)
 
     async def help(self, message: Message) -> None:
         telegram_id = self.__get_telegram_id(message)
         await self.__bot.send_message(telegram_id, message_text_help(),
-                                      reply_markup=buttons, parse_mode=ParseMode.HTML)
+                                      reply_markup=self.button_configuration.telegram_buttons(),
+                                      parse_mode=ParseMode.HTML)
 
     def __get_telegram_id(self, message: Message) -> str:
         return str(message.from_user.id)
