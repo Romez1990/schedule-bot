974ea37c1532cfd854a8840c84674eea030c746d
Romez <Romez1990@gmail.com>
2020-10-11 20:20:32 +0300

    refactor(http_client): update http_client to work with new container

--- src/http_client/module.py
+++ src/http_client/module.py
@@ -1,8 +1,8 @@
-from src.ioc_container import Module, ContainerBuilder
+from src.ioc_container import Module, Container
 from .async_http_client import AsyncHttpClient
 from .aio_http_client import AioHttpClient
 
 
 class HttpModule(Module):
-    def _load(self, builder: ContainerBuilder) -> None:
-        builder.bind(AioHttpClient).to(AsyncHttpClient)
+    def _load(self, container: Container) -> None:
+        container.bind(AioHttpClient).to(AsyncHttpClient)
