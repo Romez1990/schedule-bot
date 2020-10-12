f010a0aef42113a5554a49ec2e03d069894348c7
Iglek Kuat <dizinnes228@gmail.com>
2020-10-12 13:30:48 +0600

    feat(bot): add platform initialization buttons to constructor

--- src/platform_services/button_configuration.py
+++ src/platform_services/button_configuration.py
@@ -9,16 +9,17 @@ from vkwave.bots import (
 
 
 class ButtonConfiguration:
+    def __init__(self) -> None:
+        self.MENU_VK = Keyboard()
+        self.TELEGRAM_MENU = ReplyKeyboardMarkup()
+
     def vk_buttons(self) -> Keyboard:
-        MENU_VK = Keyboard()
-        MENU_VK.add_text_button(text='/start', payload={"command": "start"})
-        MENU_VK.add_text_button(text='/help', payload={"command": "help"})
-        return MENU_VK
+        self.MENU_VK.add_text_button(text='/start', payload={"command": "start"})
+        self.MENU_VK.add_text_button(text='/help', payload={"command": "help"})
+        return self.MENU_VK
 
     def telegram_buttons(self) -> ReplyKeyboardMarkup:
         BUTTON_START = KeyboardButton('/start')
         BUTTON_HELP = KeyboardButton('/help')
-
-        BUTTONS = ReplyKeyboardMarkup()
-        BUTTONS.add(BUTTON_START, BUTTON_HELP)
-        return BUTTONS
+        self.TELEGRAM_MENU.add(BUTTON_START, BUTTON_HELP)
+        return self.TELEGRAM_MENU
