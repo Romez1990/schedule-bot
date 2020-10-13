9ed755a43ed8c772f7ac7825693faff3d7c59209
Iglek Kuat <dizinnes228@gmail.com>
2020-10-13 21:11:47 +0600

    refactor(text_messages): add return type

--- src/platform_services/text_messages/telegram_bot_messages_interface.py
+++ src/platform_services/text_messages/telegram_bot_messages_interface.py
@@ -1,15 +1,15 @@
 class TelegramBotMessagesInterface:
-    def __telegram_message_text_start(self):
+    def __telegram_message_text_start(self) -> str:
         raise NotImplementedError
 
-    def __telegram_message_text_help(self):
+    def __telegram_message_text_help(self) -> str:
         raise NotImplementedError
 
-    def __telegram_message_text_subscribe(self):
+    def __telegram_message_text_subscribe(self) -> str:
         raise NotImplementedError
 
-    def __telegram_message_text_unsubscribe(self):
+    def __telegram_message_text_unsubscribe(self) -> str:
         raise NotImplementedError
 
-    def __telegram_message_text_theme_selection(self):
+    def __telegram_message_text_theme_selection(self) -> str:
         raise NotImplementedError
--- src/platform_services/text_messages/vk_bot_messages_interface.py
+++ src/platform_services/text_messages/vk_bot_messages_interface.py
@@ -1,15 +1,15 @@
 class VkBotMessagesInterface:
-    def __vk_message_text_start(self):
+    def __vk_message_text_start(self) -> str:
         raise NotImplementedError
 
-    def __vk_message_text_help(self):
+    def __vk_message_text_help(self) -> str:
         raise NotImplementedError
 
-    def __vk_message_text_subscribe(self):
+    def __vk_message_text_subscribe(self) -> str:
         raise NotImplementedError
 
-    def __vk_message_text_unsubscribe(self):
+    def __vk_message_text_unsubscribe(self) -> str:
         raise NotImplementedError
 
-    def __vk_message_text_theme_selection(self):
+    def __vk_message_text_theme_selection(self) -> str:
         raise NotImplementedError
