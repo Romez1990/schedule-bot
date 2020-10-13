dfe4413521d5d92beb88a2ffa438b254bd67e7ec
Iglek Kuat <dizinnes228@gmail.com>
2020-10-13 21:08:08 +0600

    feat(platform_services): add interface for vk bot messages

---
+++ src/platform_services/text_messages/vk_bot_messages_interface.py
@@ -0,0 +1,15 @@
+class VkBotMessagesInterface:
+    def __vk_message_text_start(self):
+        raise NotImplementedError
+
+    def __vk_message_text_help(self):
+        raise NotImplementedError
+
+    def __vk_message_text_subscribe(self):
+        raise NotImplementedError
+
+    def __vk_message_text_unsubscribe(self):
+        raise NotImplementedError
+
+    def __vk_message_text_theme_selection(self):
+        raise NotImplementedError
