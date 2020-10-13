42d703ec4a6279c0126228d863574bf248e17e47
Romez <Romez1990@gmail.com>
2020-10-13 18:42:21 +0300

    fix(platform_services): fix buttons

--- src/platform_services/button_configuration.py
+++ src/platform_services/button_configuration.py
@@ -9,16 +9,14 @@ from vkwave.bots import (
 
 
 class ButtonConfiguration:
-    def __init__(self) -> None:
-        self.MENU_VK = Keyboard()
-        self.TELEGRAM_MENU = ReplyKeyboardMarkup()
-
     def vk_buttons(self) -> Keyboard:
+        self.MENU_VK = Keyboard()
         self.MENU_VK.add_text_button(text='/start', payload={"command": "start"})
         self.MENU_VK.add_text_button(text='/help', payload={"command": "help"})
         return self.MENU_VK
 
     def telegram_buttons(self) -> ReplyKeyboardMarkup:
+        self.TELEGRAM_MENU = ReplyKeyboardMarkup()
         BUTTON_START = KeyboardButton('/start')
         BUTTON_HELP = KeyboardButton('/help')
         self.TELEGRAM_MENU.add(BUTTON_START, BUTTON_HELP)
