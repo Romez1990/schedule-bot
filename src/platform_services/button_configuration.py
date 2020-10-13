863c34e70241bf4a3589c2473edb365bdd74952d
Iglek Kuat <dizinnes228@gmail.com>
2020-10-13 20:05:37 +0600

    fix(bot_configuration): buttons are not crated many times

--- src/platform_services/button_configuration.py
+++ src/platform_services/button_configuration.py
@@ -9,15 +9,16 @@ from vkwave.bots import (
 
 
 class ButtonConfiguration:
+
     def vk_buttons(self) -> Keyboard:
-        self.MENU_VK = Keyboard()
-        self.MENU_VK.add_text_button(text='/start', payload={"command": "start"})
-        self.MENU_VK.add_text_button(text='/help', payload={"command": "help"})
-        return self.MENU_VK
+        MENU_VK = Keyboard()
+        MENU_VK.add_text_button(text='/start', payload={"command": "start"})
+        MENU_VK.add_text_button(text='/help', payload={"command": "help"})
+        return MENU_VK
 
     def telegram_buttons(self) -> ReplyKeyboardMarkup:
-        self.TELEGRAM_MENU = ReplyKeyboardMarkup()
+        TELEGRAM_MENU = ReplyKeyboardMarkup()
         BUTTON_START = KeyboardButton('/start')
         BUTTON_HELP = KeyboardButton('/help')
-        self.TELEGRAM_MENU.add(BUTTON_START, BUTTON_HELP)
-        return self.TELEGRAM_MENU
+        TELEGRAM_MENU.add(BUTTON_START, BUTTON_HELP)
+        return TELEGRAM_MENU
