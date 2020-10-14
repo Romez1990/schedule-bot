8ed4623ec1dc44af6ce2424d08527d0c9ff0ba3a
Iglek Kuat <dizinnes228@gmail.com>
2020-10-14 18:07:20 +0600

    feat(platform_services): add parameters in functions for vk

--- src/platform_services/text_messages/vk_bot_messages.py
+++ src/platform_services/text_messages/vk_bot_messages.py
@@ -7,16 +7,16 @@ class VkBotMessages(VkBotMessagesInterface):
         self.bot_text_messages = bot_text_messages
 
     def __vk_message_text_start(self) -> str:
-        return self.bot_text_messages.message_text_start()  # Will need add to argument
+        return self.bot_text_messages.message_text_start()
 
     def __vk_message_text_help(self) -> str:
-        return self.bot_text_messages.message_text_help()  # Will need add to argument
+        return self.bot_text_messages.message_text_help()
 
-    def __vk_message_text_subscribe(self) -> str:
-        return self.bot_text_messages.message_subscribe()  # Will need add to argument
+    def __vk_message_text_subscribe(self, username_group: str) -> str:
+        return self.bot_text_messages.message_subscribe(username_group)  # Will need add to argument
 
-    def __vk_message_text_unsubscribe(self) -> str:
-        return self.bot_text_messages.message_unsubscribe()  # Will need add to argument
+    def __vk_message_text_unsubscribe(self, username_group: str) -> str:
+        return self.bot_text_messages.message_unsubscribe(username_group)  # Will need add to argument
 
-    def __vk_message_text_theme_selection(self) -> str:
-        return self.bot_text_messages.message_theme()  # Will need add to argument
+    def __vk_message_text_theme_selection(self, group: str) -> str:
+        return self.bot_text_messages.message_theme(group)  # Will need add to argument
--- src/platform_services/text_messages/vk_bot_messages_interface.py
+++ src/platform_services/text_messages/vk_bot_messages_interface.py
@@ -5,11 +5,11 @@ class VkBotMessagesInterface:
     def __vk_message_text_help(self) -> str:
         raise NotImplementedError
 
-    def __vk_message_text_subscribe(self) -> str:
+    def __vk_message_text_subscribe(self, username_group: str) -> str:
         raise NotImplementedError
 
-    def __vk_message_text_unsubscribe(self) -> str:
+    def __vk_message_text_unsubscribe(self, username_group: str) -> str:
         raise NotImplementedError
 
-    def __vk_message_text_theme_selection(self) -> str:
+    def __vk_message_text_theme_selection(self, group: str) -> str:
         raise NotImplementedError
