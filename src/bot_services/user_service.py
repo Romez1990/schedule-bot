8fb39afc405a88e4b7e6815e73adc9b9cf5a1479
Romez <Romez1990@gmail.com>
2020-10-18 13:29:55 +0300

    fix(user_service): fix type error

--- src/bot_services/user_service.py
+++ src/bot_services/user_service.py
@@ -28,7 +28,7 @@ class UserService(UserServiceInterface):
         await self.__user_settings_service.create_default_settings(user)
 
     async def find_user(self, platform_id: str, *,
-                        find_settings=False, find_subscriptions=False) -> User:
+                        find_settings: bool = False, find_subscriptions: bool = False) -> User:
         user = (await self.__users.find(self.__platform, platform_id)).unwrap()
         user_settings = (await self.__user_settings_service.find(user)) if find_settings else None
         subscriptions = (await self.__subscription_service.find(user)) if find_subscriptions else None
