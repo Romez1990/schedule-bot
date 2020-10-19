761b6ad84cb6527f8dded8094634553c64a4e7a9
Iglek Kuat <dizinnes228@gmail.com>
2020-10-19 21:33:59 +0600

    refactor(platform_service): rewrite vk controller ad telegram controller

--- src/platform_services/vk_service/vk_controller.py
+++ src/platform_services/vk_service/vk_controller.py
@@ -6,8 +6,10 @@ from src.bot_services import (
     SubscriptionServiceInterface,
 )
 
+from returns.unsafe import unsafe_perform_io
+
 from ..button_configuration import ButtonConfiguration
-from ..messages_text import MessageText
+from ..text_messages import TextMessagesFactoryInterface
 from .vk_bot import VkBot
 
 
@@ -15,27 +17,50 @@ class VkController:
     def __init__(
             self,
             bot: VkBot,
-            vk_button: ButtonConfiguration,
-            message_text: MessageText,
+            button_configuration: ButtonConfiguration,
+            text_messages_factory: TextMessagesFactoryInterface,
+            message_text: TextMessagesFactoryInterface,
             user_service_factory: UserServiceFactoryInterface,
             user_settings_service: UserSettingsServiceInterface,
             subscription_service: SubscriptionServiceInterface,
     ) -> None:
         self.__bot = bot
-        self.__vk_button = vk_button
+        self.__button_configuration = button_configuration
         self.__message_text = message_text
-        self.__button = vk_button.vk_buttons()
+        self.__text_messages = text_messages_factory.create_html_text_messages()
         self.__user_service = user_service_factory.create('vk')
         self.__user_settings_service = user_settings_service
         self.__subscription_service = subscription_service
 
     async def welcome(self, event: SimpleBotEvent) -> None:
-        await event.answer(message=self.__message_text.message_text_start('vk'),
-                           keyboard=self.__button.get_keyboard())
+        vk_id = self.__get_vk_id(event)
+        await self.__user_service.create_if_not_exists(vk_id)
+        await event.answer(message=self.__message_text.create_plain_text_messages(),
+                           keyboard=self.__button_configuration.vk_buttons())
 
     async def help(self, event: SimpleBotEvent) -> None:
-        await event.answer(message=self.__message_text.message_text_help('vk'),
-                           keyboard=self.__button.get_keyboard())
+        await event.answer(message=self.__message_text.create_plain_text_messages(),
+                           keyboard=self.__button_configuration.vk_buttons())
+
+    async def subscribe(self, event: SimpleBotEvent) -> None:
+        vk_id = self.__get_telegram_id(event)
+        user = await self.__user_service.find_user(vk_id)
+        group_name = event.text.lstrip('/subscribe ')
+        result = await (self.__subscription_service.create(user, group_name)
+                        .map(lambda _: self.__bot.send_message(vk_id, self.__text_messages.subscribe(group_name)))
+                        .fix(lambda _: self.__bot.send_message(vk_id, 'wrong group'))
+                        .awaitable())
+        await unsafe_perform_io(result.unwrap())
+
+    async def unsubscribe(self, event: SimpleBotEvent) -> None:
+        vk_id = self.__get_telegram_id(event)
+        user = await self.__user_service.find_user(vk_id)
+        group_name = event.text.lstrip('/unsubscribe ')
+        result = await (self.__subscription_service.delete(user, group_name)
+                        .map(lambda _: self.__bot.send_message(vk_id, self.__text_messages.unsubscribe(group_name)))
+                        .fix(lambda _: self.__bot.send_message(vk_id, 'wrong group'))
+                        .awaitable())
+        await unsafe_perform_io(result.unwrap())
 
     def __get_vk_id(self, event: SimpleBotEvent) -> str:
         return str(event.object.object.message.from_id)
