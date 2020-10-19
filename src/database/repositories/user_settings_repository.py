7a212107d61f15b12bdd3b91686d8b524845caaf
Romez <Romez1990@gmail.com>
2020-10-19 19:26:26 +0300

    refactor(entities): change theme parameter in user settings from string to theme type

--- src/database/repositories/user_settings_repository.py
+++ src/database/repositories/user_settings_repository.py
@@ -1,17 +1,21 @@
 from src.entities import UserSettings, User
+from src.schedule_services import (
+    ThemeRepositoryInterface,
+)
 from ..database import Database
 from .user_settings_repository_interface import UserSettingsRepositoryInterface
 
 
 class UserSettingsRepository(UserSettingsRepositoryInterface):
-    def __init__(self, database: Database) -> None:
+    def __init__(self, database: Database, themes: ThemeRepositoryInterface) -> None:
         self.__database = database
+        self.__themes = themes
 
     async def save(self, user_settings: UserSettings) -> UserSettings:
         await self.__database.execute('''
             INSERT INTO user_settings(user_id, theme)
             VALUES ($1, $2)
-        ''', user_settings.user.id, user_settings.theme)
+        ''', user_settings.user.id, user_settings.theme.name)
         return user_settings
 
     async def delete(self, user_settings: UserSettings) -> None:
@@ -25,11 +29,13 @@ class UserSettingsRepository(UserSettingsRepositoryInterface):
             UPDATE user_settings
             SET theme = $2
             WHERE user_id = $1 
-        ''', user_settings.user.id, user_settings.theme)
+        ''', user_settings.user.id, user_settings.theme.name)
 
     async def find_by_user(self, user: User) -> UserSettings:
         maybe_user_settings_record = await self.__database.fetch_row('''
             SELECT theme FROM user_settings
             WHERE user_id = $1
         ''', user.id)
-        return UserSettings(user, **maybe_user_settings_record.unwrap())
+        user_settings_record = maybe_user_settings_record.unwrap()
+        theme = self.__themes.get_by_name(user_settings_record.pop('theme')).unwrap()
+        return UserSettings(user, theme)
--- src/entities/user_settings.py
+++ src/entities/user_settings.py
@@ -1,8 +1,11 @@
+from src.schedule_services import (
+    Theme,
+)
 from .user import User
 
 
 class UserSettings:
-    def __init__(self, user: User, theme: str) -> None:
+    def __init__(self, user: User, theme: Theme) -> None:
         self.__user = user
         self.__theme = theme
 
@@ -11,5 +14,5 @@ class UserSettings:
         return self.__user
 
     @property
-    def theme(self) -> str:
+    def theme(self) -> Theme:
         return self.__theme
