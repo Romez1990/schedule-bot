2f36448ef4803fda0d1af76986d13472d9eec750
Romez <Romez1990@gmail.com>
2020-10-20 19:34:10 +0300

    refactor(schedule_services): refactor theme repository

--- src/schedule_services/renderer/themes/theme_repository.py
+++ src/schedule_services/renderer/themes/theme_repository.py
@@ -1,18 +1,22 @@
-from returns.maybe import Maybe
+from typing import (
+    Mapping,
+)
+from returns.maybe import Maybe, Some, Nothing
 
-from src.utilities import ListHelper
 from .theme_repository_interface import ThemeRepositoryInterface
 from .theme import Theme
 from .color import Color
 
 
 class ThemeRepository(ThemeRepositoryInterface):
-    def __init__(self, list_helper: ListHelper) -> None:
-        self.__list_helper = list_helper
-        self.__themes = [
+    def __init__(self) -> None:
+        themes = [
             Theme('light', Color(0, 0, 0), [Color(255, 255, 255), Color(238, 238, 238)]),
             Theme('dark', Color(173, 173, 173), [Color(37, 37, 38), Color(32, 32, 33)]),
         ]
+        self.__themes: Mapping[str, Theme] = {theme.name: theme for theme in themes}
 
     def get_by_name(self, name: str) -> Maybe[Theme]:
-        return self.__list_helper.find_first(self.__themes, lambda theme: theme.name == name)
+        if name not in self.__themes:
+            return Nothing
+        return Some(self.__themes[name])
