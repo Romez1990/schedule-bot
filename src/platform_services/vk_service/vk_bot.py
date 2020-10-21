58416b602d362fb3b9fae52121bf1a3ac708fe7e
Iglek Kuat <dizinnes228@gmail.com>
2020-10-22 00:53:34 +0600

    refactor(platform_service): transfer to library vkbottle

--- src/platform_services/vk_service/vk_bot.py
+++ src/platform_services/vk_service/vk_bot.py
@@ -12,24 +12,39 @@ from vkwave.bots.core import (
 
 from src.env import EnvironmentInterface
 
+#  vkbottle
+from vkbottle.bot import (
+    Bot,
+)
+
+from vkbottle.rule import (
+    filters
+)
+
+
 
 class VkBot:
     def __init__(self, env: EnvironmentInterface) -> None:
         self.__env = env
 
-    __bot: SimpleLongPollUserBot
+    __bot: Bot
 
     def init(self) -> None:
         token = self.__env.get_str('VK_BOT_TOKEN')
-        self.__bot = SimpleLongPollUserBot(tokens=token)
-
+        self.__bot = Bot(tokens=token)
+                                                                                                                    # КОРОЧЕ ПЫТАЛСЯ НО НЕ ПОЛУЧИЛОСЬ, F   (C) DiZiNnEs
     def message_handler(self, *filters: BaseFilter) -> Callable:
-        return self.__bot.message_handler(*filters)
+        return self.__bot.on.message_handler(*filters)
+
+    # def command_filter(self, commands: str,
+    #                    prefixes: Tuple[str, ...] = ("/", "!"),
+    #                    ignore_case: bool = True) -> CommandsFilter:
+    #     return self.__bot.command_filter(commands, prefixes, ignore_case)
 
     def command_filter(self, commands: str,
                        prefixes: Tuple[str, ...] = ("/", "!"),
                        ignore_case: bool = True) -> CommandsFilter:
-        return self.__bot.command_filter(commands, prefixes, ignore_case)
+        return filters.all_filter()
 
     async def start(self) -> None:
         await self.__bot.run()
