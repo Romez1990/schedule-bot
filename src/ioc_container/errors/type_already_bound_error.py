6dedc8ae7e96c9bfc425e3106f05c827adf6f63b
Romez <Romez1990@gmail.com>
2020-12-27 14:49:49 +0300

    fix(ioc_container): fix typo in error message

--- src/ioc_container/errors/type_already_bound_error.py
+++ src/ioc_container/errors/type_already_bound_error.py
@@ -5,4 +5,4 @@ from typing import (
 
 class TypeAlreadyBoundError(Exception):
     def __init__(self, base_type: Type, type: Type) -> None:
-        super().__init__(f'type "{base_type.__name__}" is already bound with "{type.__name__}"')
+        super().__init__(f'type "{base_type.__name__}" is already bound to "{type.__name__}"')
