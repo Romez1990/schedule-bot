4e3bfb7fc474ef4c67926d0180d0e002b15a32cf
Romez <Romez1990@gmail.com>
2021-08-02 19:03:34 +0300

    feat(vector): crete list

---
+++ src/data/vector/list.py
@@ -0,0 +1,91 @@
+from __future__ import annotations
+from functools import reduce
+from collections.abc import (
+    Iterable,
+    Sequence,
+)
+from typing import (
+    Optional,
+    Callable,
+    Type,
+    TypeVar,
+    overload,
+)
+
+from data.fp.type import cast
+
+T = TypeVar('T')
+T2 = TypeVar('T2')
+
+
+class List(Sequence[T]):
+    def __init__(self, iterable: Iterable[T] = None) -> None:
+        self.__list = self.__create_list(iterable)
+
+    def __create_list(self, iterable: Optional[Iterable[T]]) -> list[T]:
+        if iterable is None:
+            return []
+        if isinstance(iterable, List):
+            return iterable.__list
+        return list(iterable)
+
+    @overload
+    def __getitem__(self, index: int) -> T: ...
+
+    @overload
+    def __getitem__(self, slice_: slice) -> Sequence[T]: ...
+
+    def __getitem__(self, index_or_slice: int | slice) -> T | Sequence[T]:
+        result = self.__list[index_or_slice]
+        if not isinstance(result, list):
+            return result
+        return List(result)
+
+    def __len__(self) -> int:
+        return len(self.__list)
+
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, List):
+            return False
+        return self.__list == other.__list
+
+    def cast(self, new_type: Type[T2]) -> List[T2]:
+        return self.map(cast(new_type))
+
+    def map(self, function: Callable[[T], T2]) -> List[T2]:
+        return List(map(function, self))
+
+    def filter(self, function: Callable[[T], bool]) -> List[T]:
+        return List(filter(function, self))
+
+    @overload
+    def reduce(self, function: Callable[[T, T], T]) -> T: ...
+
+    @overload
+    def reduce(self, function: Callable[[T2, T], T2], initial: T2) -> T2: ...
+
+    def reduce(self, function: Callable, initial: T2 = None) -> T | T2:
+        if initial is None:
+            return reduce(function, self)
+        return reduce(function, self, initial)
+
+    def add(self, element: T) -> List[T]:
+        new_list = self.__list.copy()
+        new_list.append(element)
+        return List(new_list)
+
+    def remove(self, element: T) -> List[T]:
+        new_list = self.__list.copy()
+        try:
+            new_list.remove(element)
+        except ValueError:
+            raise ValueError(f'{element} not in list')
+        return List(new_list)
+
+    def pop(self, index: int = None) -> tuple[T, List[T]]:
+        new_list = self.__list.copy()
+        element = new_list.pop() if index is None else new_list.pop(index)
+        return element, List(new_list)
+
+    def __str__(self) -> str:
+        return str(self.__list)
---
+++ test/data/vector/list_test.py
@@ -0,0 +1,85 @@
+from pytest import raises
+
+from data.vector import List
+
+
+def test_eq__equal_lists__returns_true() -> None:
+    list1 = List([1, 2, 3])
+    list2 = List([1, 2, 3])
+
+    result = list1 == list2
+
+    assert result
+
+
+def test_eq__different_lists_different_length__returns_false() -> None:
+    list1 = List([1, 2, 3])
+    list2 = List([4, 3, 2, 1])
+
+    result = list1 == list2
+
+    assert not result
+
+
+def test_eq__different_lists_same_length__returns_false() -> None:
+    list1 = List([1, 2, 3])
+    list2 = List([3, 2, 1])
+
+    result = list1 == list2
+
+    assert not result
+
+
+def test_ne__equal_lists_returns__false() -> None:
+    list1 = List([1, 2, 3])
+    list2 = List([1, 2, 3])
+
+    result = list1 != list2
+
+    assert not result
+
+
+def test_ne__different_lists_different_length__returns_true() -> None:
+    list1 = List([1, 2, 3])
+    list2 = List([4, 3, 2, 1])
+
+    result = list1 != list2
+
+    assert result
+
+
+def test_ne__different_lists_same_length__returns_true() -> None:
+    list1 = List([1, 2, 3])
+    list2 = List([3, 2, 1])
+
+    result = list1 != list2
+
+    assert result
+
+
+def test_add__element__contains_new_element() -> None:
+    old_list = List([11, 22, 33])
+
+    new_list = old_list.add(44)
+
+    assert 44 in new_list
+    assert list(new_list).index(44) == 3
+    assert len(new_list) == len(old_list) + 1
+
+
+def test_remove__element__not_contain_element() -> None:
+    old_list = List([11, 22, 33])
+
+    new_list = old_list.remove(22)
+
+    assert 22 not in new_list
+    assert len(new_list) == len(old_list) - 1
+
+
+def test_remove__not_existing_element__returns_same_list() -> None:
+    old_list = List([11, 22, 33])
+
+    with raises(ValueError) as e:
+        old_list.remove(99)
+
+    assert str(e.value) == '99 not in list'
