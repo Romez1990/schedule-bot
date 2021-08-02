697f2f77af38a190fa7fca3aef3b0cd6da1ee064
Romez <Romez1990@gmail.com>
2021-08-02 19:03:40 +0300

    feat(vector): crete list helper

---
+++ src/data/vector/list_helpers.py
@@ -0,0 +1,27 @@
+from functools import reduce
+from typing import (
+    Callable,
+    TypeVar,
+    overload,
+)
+
+from .list import List
+
+A = TypeVar('A')
+B = TypeVar('B')
+
+
+@overload
+def lazy_reduce(function: Callable[[A, Callable[[], A]], A], lazy_list: List[Callable[[], A]]) -> A: ...
+
+
+@overload
+def lazy_reduce(function: Callable[[B, Callable[[], A]], B], lazy_list: List[Callable[[], A]],
+                initial: B) -> B: ...
+
+
+def lazy_reduce(function: Callable, lazy_list: List[Callable[[], A]], initial: B = None) -> A | B:
+    if initial is not None:
+        return reduce(function, lazy_list, initial)
+    head, tail = lazy_list.pop(0)
+    return reduce(function, tail, head())
