0883f30ba191c396091fa8d89554f56ac85abc11
Romez <Romez1990@gmail.com>
2021-08-03 11:04:46 +0300

    feat(start): add start message in start script

--- scripts/start.py
+++ scripts/start.py
@@ -1,13 +1,15 @@
 from infrastructure.script import AsyncScript, script
+from infrastructure.logger import LoggerFactory
 from messenger_services.messenger_service import MessageHandlerRegistrar
 
 
 @script
 class StartScript(AsyncScript):
-    def __init__(self, message_handler_registrar: MessageHandlerRegistrar) -> None:
+    def __init__(self, logger_factory: LoggerFactory, message_handler_registrar: MessageHandlerRegistrar) -> None:
+        self.logger = logger_factory.create('scripts.start')
         self.message_handler_registrar = message_handler_registrar
 
     async def run(self) -> None:
         self.message_handler_registrar.register(self.container)
-        print('App has been started')
+        self.logger.info('App has been started')
         await self.message_handler_registrar.start()
