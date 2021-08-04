767f0e3889e877a26284619b6ad3c0d52aed3467
Romez <Romez1990@gmail.com>
2021-08-04 10:18:21 +0300

    feat(database): create data fetcher abstract class

---
+++ src/storage/database/data_fetcher.py
@@ -0,0 +1,30 @@
+from abc import ABCMeta, abstractmethod
+from typing import (
+    Type,
+    TypeVar,
+)
+
+from data.fp.maybe import Maybe
+from data.fp.task_either import TaskEither
+from .errors import (
+    DatabaseError,
+)
+
+Record = dict[str, object]
+Records = list[Record]
+
+T = TypeVar('T')
+
+
+class DataFetcher(metaclass=ABCMeta):
+    @abstractmethod
+    def execute(self, query: str, *args: object) -> TaskEither[DatabaseError, None]: ...
+
+    @abstractmethod
+    def fetch(self, query: str, *args: object) -> TaskEither[DatabaseError, Records]: ...
+
+    @abstractmethod
+    def fetch_row(self, query: str, *args: object) -> TaskEither[DatabaseError, Maybe[Record]]: ...
+
+    @abstractmethod
+    def fetch_value(self, query: str, *args: object, value_type: Type[T]) -> TaskEither[DatabaseError, T]: ...
