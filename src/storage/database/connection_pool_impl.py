d4dd493eaa482119e6aebfeb3ed2b955bda36e8d
Romez <Romez1990@gmail.com>
2022-03-17 22:14:13 +0300

    temp

--- src/storage/database/connection_pool_impl.py
+++ src/storage/database/connection_pool_impl.py
@@ -1,17 +1,17 @@
 from asyncio import (
     Lock,
-    Future,
     TimeoutError,
+    Queue,
     wait_for,
 )
-from queue import Queue
+from itertools import chain
 from typing import (
+    Callable,
     cast,
 )
 
 from infrastructure.ioc_container import service
 from infrastructure.config import Config
-from data.fp.maybe import Maybe
 from .errors import (
     GetConnectionTimeoutError,
 )
@@ -29,22 +29,21 @@ class ConnectionPoolImpl(ConnectionPool):
         self.__max_size = config.db_connection_pool_max_size
         self.__get_connection_timeout = config.db_connection_pool_timeout
         self.__used_connections: list[ManageablePoolConnection] = []
-        self.__unused_connections: list[ManageablePoolConnection] = []
-        self.__connection_waiters: Queue[Future[PoolConnection]] = Queue()
+        self.__unused_connections: Queue[ManageablePoolConnection] = Queue()
         self.__get_connection_lock = Lock()
 
     async def init(self) -> None:
-        await self.__create_connection_and_add_to(self.__unused_connections)
+        await self.__create_connection_and_add(self.__unused_connections.put_nowait)
 
     async def destroy(self) -> None:
-        connections = self.__used_connections + self.__unused_connections
+        connections = chain(self.__used_connections, self.__unused_connections)
         for connection in connections:
             await connection.close()
 
     def get_connection(self) -> PoolConnectionContextManager:
         async def async_get_connection() -> PoolConnection:
             await self.__get_connection_lock.acquire()
-            unused_is_empty = len(self.__unused_connections) == 0
+            unused_is_empty = self.__unused_connections.empty()
             if unused_is_empty and len(self.__used_connections) < self.__max_size:
                 return await self.__create_connection()
             if unused_is_empty and len(self.__used_connections) == self.__max_size:
@@ -55,46 +54,34 @@ class ConnectionPoolImpl(ConnectionPool):
         return PoolConnectionContextManager(async_get_connection())
 
     def __get_unused_connection(self) -> PoolConnection:
-        connection = self.__unused_connections.pop()
+        connection = self.__unused_connections.get_nowait()
         self.__used_connections.append(connection)
         self.__get_connection_lock.release()
         return connection
 
     async def __create_connection(self) -> PoolConnection:
-        return await self.__create_connection_and_add_to(self.__used_connections)
+        return await self.__create_connection_and_add(self.__used_connections.append)
 
-    async def __create_connection_and_add_to(self, collection: list[ManageablePoolConnection]) -> PoolConnection:
+    async def __create_connection_and_add(self, add_to_collection: Callable[[ManageablePoolConnection], None]
+                                          ) -> PoolConnection:
         def release_connection() -> None:
             self.release_connection(connection)
 
         connection = self.__connection_factory.create(release_connection)
-        collection.append(connection)
+        add_to_collection(connection)
         if self.__get_connection_lock.locked():
             self.__get_connection_lock.release()
         await connection.open()
         return connection
 
     async def __wait_for_connection(self) -> PoolConnection:
-        future: Future[PoolConnection] = Future()
-        self.__connection_waiters.put(future)
         try:
-            connection = await wait_for(future, self.__get_connection_timeout)
+            connection = await wait_for(self.__unused_connections.get(), self.__get_connection_timeout)
         except TimeoutError:
             raise GetConnectionTimeoutError(self.__get_connection_timeout)
         return connection
 
     def release_connection(self, connection: PoolConnection) -> None:
-        def get_connection_waiter() -> Future[PoolConnection]:
-            return self.__connection_waiters.get()
-
-        def pass_connection(connection_waiter: Future[PoolConnection]) -> None:
-            # is doesn't move from used_connections to unused_connections because connection is passed to another use
-            connection_waiter.set_result(connection)
-
-        def release_connection() -> None:
-            manageable_pool_connection = cast(ManageablePoolConnection, connection)
-            self.__used_connections.remove(manageable_pool_connection)
-            self.__unused_connections.append(manageable_pool_connection)
-
-        Maybe.try_except(get_connection_waiter, IndexError) \
-            .match(release_connection, pass_connection)
+        manageable_pool_connection = cast(ManageablePoolConnection, connection)
+        self.__used_connections.remove(manageable_pool_connection)
+        self.__unused_connections.put_nowait(manageable_pool_connection)
