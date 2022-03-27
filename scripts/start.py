b1b9bf283a4032a858c6ad23f96d5fcb35c564c0
Romez <Romez1990@gmail.com>
2022-03-27 20:40:05 +0300

    temp

--- scripts/start.py
+++ scripts/start.py
@@ -1,15 +1,19 @@
 from infrastructure.script_runner import AsyncScript, script
 from infrastructure.logger import LoggerFactory
+from storage.database import ConnectionPool
 from messenger_services.messenger_service import MessageHandlerRegistrar
 
 
 @script
 class StartScript(AsyncScript):
-    def __init__(self, logger_factory: LoggerFactory, message_handler_registrar: MessageHandlerRegistrar) -> None:
+    def __init__(self, logger_factory: LoggerFactory, connection_pool: ConnectionPool,
+                 message_handler_registrar: MessageHandlerRegistrar) -> None:
         self.logger = logger_factory.create()
+        self.connection_pool = connection_pool
         self.message_handler_registrar = message_handler_registrar
 
     async def run(self) -> None:
+        await self.connection_pool.init()
         self.message_handler_registrar.register(self.container)
         self.logger.info('App has been started')
         await self.message_handler_registrar.start()
