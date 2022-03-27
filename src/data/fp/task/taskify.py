b109b516dec0fcfc8b1174d3fe0cd73f1d361896
Romez <Romez1990@gmail.com>
2022-03-27 08:57:05 +0300

    temp

--- src/data/fp/task/taskify.py
+++ src/data/fp/task/taskify.py
@@ -2,15 +2,17 @@ from typing import (
     Awaitable,
     Callable,
     TypeVar,
+    ParamSpec,
 )
 
 from .task import Task
 
+P = ParamSpec('P')
 T = TypeVar('T')
 
 
-def taskify(fn: Callable[[...], Awaitable[T]]) -> Callable[[...], Task[T]]:  # type: ignore
-    def wrapper(*args: object, **kwargs: object) -> Task[T]:
+def taskify(fn: Callable[P, Awaitable[T]]) -> Callable[P, Task[T]]:
+    def wrapper(*args: P.args, **kwargs: P.kwargs) -> Task[T]:
         return Task(fn(*args, **kwargs))  # type: ignore
 
     return wrapper
