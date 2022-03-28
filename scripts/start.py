31702043e4047d6b16c419946793c6920cf5fb30
Romez <Romez1990@gmail.com>
2022-03-28 21:12:50 +0300

    temp

--- scripts/start.py
+++ scripts/start.py
@@ -1,5 +1,10 @@
+from typing import (
+    NoReturn,
+)
+
 from infrastructure.script_runner import AsyncScript, script
 from infrastructure.logger import LoggerFactory
+from infrastructure.errors import NoReturnError
 from storage.database import ConnectionPool
 from messenger_services.messenger_service import MessageHandlerRegistrar
 
@@ -12,8 +17,9 @@ class StartScript(AsyncScript):
         self.connection_pool = connection_pool
         self.message_handler_registrar = message_handler_registrar
 
-    async def run(self) -> None:
+    async def run(self) -> NoReturn:
         await self.connection_pool.init()
         self.message_handler_registrar.register(self.container)
         self.logger.info('App has been started')
-        await self.message_handler_registrar.start()
+        self.message_handler_registrar.start(),
+        raise NoReturnError
