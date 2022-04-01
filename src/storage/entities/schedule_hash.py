e9105b603af48e5772bffb3f0a5730e5b531f280
Romez <Romez1990@gmail.com>
2022-04-01 15:26:50 +0300

    temp

--- src/storage/entities/schedule_hash.py
+++ src/storage/entities/schedule_hash.py
@@ -7,8 +7,8 @@ from .entity_base import Entity
 class ScheduleHash(Entity):
     def __init__(self, schedule_date: date, schedule_hash: int, id: int = None) -> None:
         self.id = id
-        self.date = schedule_date
+        self.starts_at = schedule_date
         self.hash = schedule_hash
 
     def set_id(self, schedule_hash_id: int) -> ScheduleHash:
-        return ScheduleHash(self.date, self.hash, schedule_hash_id)
+        return ScheduleHash(self.starts_at, self.hash, schedule_hash_id)
