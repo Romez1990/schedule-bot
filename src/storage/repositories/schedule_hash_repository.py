b00320490e4a0310b4c825aa6c559ef1f7b8bbe5
Romez <Romez1990@gmail.com>
2022-04-01 15:27:01 +0300

    temp

--- src/storage/repositories/schedule_hash_repository.py
+++ src/storage/repositories/schedule_hash_repository.py
@@ -1,8 +1,12 @@
 from abc import ABCMeta, abstractmethod
+from typing import (
+    Sequence,
+)
 
 from data.fp.task import Task
+from storage.entities import ScheduleHash
 
 
 class ScheduleHashRepository(metaclass=ABCMeta):
     @abstractmethod
-    def f(self) -> Task: ...
+    def save_all(self, schedule_hashes: Sequence[ScheduleHash]) -> Task[Sequence[ScheduleHash]]: ...
