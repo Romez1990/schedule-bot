99755c55fb209259225055c472b25f1bdb8712a9
Romez <Romez1990@gmail.com>
2022-04-03 12:43:10 +0300

    temp

--- src/storage/entities/entity_base/entity.py
+++ src/storage/entities/entity_base/entity.py
@@ -7,13 +7,13 @@ class Entity:
             raise RuntimeError('entity is readonly')
         super().__setattr__(key, value)
 
-    def __str__(self) -> str:
+    def __repr__(self) -> str:
         entity_name = self.__class__.__name__
         attributes = List(self.__dict__.items()) \
             .map(self.__join_attribute_name_and_value)
         attributes_with_comma = ', '.join(attributes)
-        return f'{entity_name} {{ {attributes_with_comma} }}'
+        return f'{entity_name}({attributes_with_comma})'
 
     def __join_attribute_name_and_value(self, item: tuple[str, object]) -> str:
         name, value = item
-        return f'{name} = {value}'
+        return f'{name}={repr(value)}'
