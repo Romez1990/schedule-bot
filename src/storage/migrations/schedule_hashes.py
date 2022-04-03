e0ac18ece0e27eb995a67e2a3b842bcb25090ede
Romez <Romez1990@gmail.com>
2022-04-03 12:44:10 +0300

    temp

--- src/storage/migrations/schedule_hashes.py
+++ src/storage/migrations/schedule_hashes.py
@@ -4,7 +4,7 @@ from storage.migration_runner import Migration, migration
 @migration
 class ScheduleHashesMigration(Migration):
     create_table = '''
-        CREATE TABLE public.schedule_hashes (
+        CREATE TABLE schedule_hashes (
             id serial PRIMARY KEY,
             starts_at date NOT NULL,
             hash int NOT NULL
--- src/storage/migrations/users.py
+++ src/storage/migrations/users.py
@@ -4,7 +4,7 @@ from storage.migration_runner import Migration, migration
 @migration
 class UsersMigration(Migration):
     create_table = '''
-        CREATE TABLE public.users (
+        CREATE TABLE users (
             id serial PRIMARY KEY,
             messenger character varying(40) NOT NULL,
             messenger_id character varying(40) NOT NULL
