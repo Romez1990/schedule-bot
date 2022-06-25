67080dc46f987a1b161316d71a4b89517202a5e6
Romez <Romez1990@gmail.com>
2022-06-25 16:52:44 +0300

    temp

--- src/schedule_services/update_checker/week_schedule_changes_determinant_impl.py
+++ src/schedule_services/update_checker/week_schedule_changes_determinant_impl.py
@@ -3,6 +3,7 @@ from typing import (
     Awaitable,
 )
 
+from infrastructure.ioc_container import service
 from schedule_services.schedule import (
     Schedule,
     Group,
@@ -10,6 +11,7 @@ from schedule_services.schedule import (
 from .week_schedule_changes_determinant import WeekScheduleChangesDeterminant
 
 
+@service
 class WeekScheduleChangesDeterminantImpl(WeekScheduleChangesDeterminant):
     def get_changed_groups(self,
                            schedules: Sequence[Schedule]) -> Awaitable[Sequence[tuple[Schedule, Sequence[Group]]]]:
