f06d10ed9d7e5c39de4a79deebe4656c070ab832
Romez <Romez1990@gmail.com>
2022-07-05 13:15:19 +0300

    drop vkwave support

--- src/messenger_services/messenger_service/message_handler_registrar_impl.py
+++ src/messenger_services/messenger_service/message_handler_registrar_impl.py
@@ -8,7 +8,7 @@ from infrastructure.errors import NoReturnError
 from data.fp.task import Task
 from data.vector import List
 from messenger_services.telegram_service import TelegramService
-from messenger_services.vk_service import VkWaveService, VkBottleService
+from messenger_services.vk_service import VkBottleService
 from .message_handler_registrar import MessageHandlerRegistrar
 from .controller_decorator import messenger_controllers
 from .message_handler_adapter import MessageHandlerAdapter
@@ -21,11 +21,10 @@ from .message_handler_decorator import message_handler_parameters
 @service
 class MessageHandlerRegistrarImpl(MessageHandlerRegistrar):
     def __init__(self, message_handler_adapter: MessageHandlerAdapter, telegram: TelegramService,
-                 vk_wave: VkWaveService, vk_bottle: VkBottleService) -> None:
+                 vk_bottle: VkBottleService) -> None:
         self.__message_handler_adapter = message_handler_adapter
         self.__messenger_services: list[MessengerService] = [
             telegram,
-            vk_wave,
             vk_bottle,
         ]
 
--- src/messenger_services/vk_service/__init__.py
+++ src/messenger_services/vk_service/__init__.py
@@ -1,2 +1,1 @@
-from .vk_wave_service import VkWaveService
 from .vk_bottle_service import VkBottleService
