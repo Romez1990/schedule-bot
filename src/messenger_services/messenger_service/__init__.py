107d90352cf83cc797f5ca70ec90f6aa4aa66761
Romez <Romez1990@gmail.com>
2022-07-05 18:09:44 +0300

    temp

--- src/messenger_services/messenger_service/__init__.py
+++ src/messenger_services/messenger_service/__init__.py
@@ -3,9 +3,9 @@ from .structures import (
     User,
     MessageHandlerParameters,
 )
+from .message_handler_registrar import MessageHandlerRegistrar
 from .messenger_service import MessengerService
-from .messenger_controller import MessengerController
 from .messenger_adapter import MessengerAdapter
-from .message_handler_registrar_impl import MessageHandlerRegistrar
+from .messenger_controller import MessengerController
 from .controller_decorator import controller
 from .message_handler_decorator import message_handler
