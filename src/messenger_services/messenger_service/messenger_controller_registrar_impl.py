644e85312651cc304b569a69b9af30be4575bf36
Romez <Romez1990@gmail.com>
2022-07-07 16:08:17 +0300

    temp

--- src/messenger_services/messenger_service/messenger_controller_registrar_impl.py
+++ src/messenger_services/messenger_service/messenger_controller_registrar_impl.py
@@ -1,11 +1,13 @@
 from typing import (
     Sequence,
+    Mapping,
     Callable,
     Type,
     Awaitable,
 )
 
 from infrastructure.ioc_container import service, Container
+from data.vector import List
 from messenger_services.telegram_service import TelegramService
 from messenger_services.vk_service import VkService
 from .structures import (
@@ -30,32 +32,49 @@ class MessengerControllerRegistrarImpl(MessengerControllerRegistrar):
         ]
 
     def register(self, container: Container) -> None:
-        controllers = self.__create_controllers(container)
-        for messenger_service in self.messenger_services:
-            controllers_for_messenger_service = controllers[type(messenger_service)]
-            for parameters in message_handler_parameters:
-                controller = controllers_for_messenger_service[parameters.controller_class]
-                self.__register_message_handler_for_messenger_service(messenger_service, controller, parameters)
+        controllers = self.__instantiate_controllers(container)
+        self.__register_controllers(controllers)
 
-    def __create_controllers(self, container: Container
-                             ) -> dict[Type[MessengerService], dict[Type[MessengerController], MessengerController]]:
-        controllers = {}
-        for messenger_service in self.messenger_services:
-            controllers_for_messenger_service = {}
-            for controller_class in messenger_controllers:
-                adapter = messenger_service.adapter
-                additional_parameters = {
-                    'adapter': adapter,
-                }
-                controller = container.instantiate(controller_class, **additional_parameters)
-                controllers_for_messenger_service[controller_class] = controller
-            controllers[type(messenger_service)] = controllers_for_messenger_service
-        return controllers
+    def __instantiate_controllers(self, container: Container
+                                  ) -> Mapping[Type[MessengerController], Sequence[MessengerController]]:
+        controllers_for_messengers = List(messenger_controllers) \
+            .map(self.__instantiate_controller(container))
+        return {controller_class: controllers
+                for controller_class, controllers in zip(messenger_controllers, controllers_for_messengers)}
 
-    def __register_message_handler_for_messenger_service(
-            self, messenger_service: MessengerService, controller: MessengerController,
-            parameters: MessageHandlerParameters) -> None:
-        adapter = messenger_service.adapter
+    def __instantiate_controller(self, container: Container
+                                 ) -> Callable[[Type[MessengerController]], Sequence[MessengerController]]:
+        def instantiate_controller(controller_class: Type[MessengerController]) -> Sequence[MessengerController]:
+            return List(self.messenger_services) \
+                .map(self.__instantiate_controller_for_messenger(container, controller_class))
+
+        return instantiate_controller
+
+    def __instantiate_controller_for_messenger(self, container: Container, controller_class: Type[MessengerController]
+                                               ) -> Callable[[MessengerService], MessengerController]:
+        def instantiate_controller_for_messenger(messenger_service: MessengerService) -> MessengerController:
+            adapter = messenger_service.adapter
+            additional_parameters = {
+                'adapter': adapter,
+            }
+            return container.instantiate(controller_class, **additional_parameters)
+
+        return instantiate_controller_for_messenger
+
+    def __register_controllers(self,
+                               controllers: Mapping[Type[MessengerController], Sequence[MessengerController]]) -> None:
+        for parameters in message_handler_parameters:
+            controllers_for_messengers = controllers[parameters.controller_class]
+            self.__register_controllers_for_messengers(controllers_for_messengers, parameters)
+
+    def __register_controllers_for_messengers(self, controllers: Sequence[MessengerController],
+                                              parameters: MessageHandlerParameters) -> None:
+        for messenger_service, controller in zip(self.messenger_services, controllers):
+            adapter = messenger_service.adapter
+            self.__register_message_handler_for_messenger(adapter, controller, parameters)
+
+    def __register_message_handler_for_messenger(self, adapter: MessengerAdapter, controller: MessengerController,
+                                                 parameters: MessageHandlerParameters) -> None:
         message_handler = self.__get_message_handler(controller, adapter, parameters.method_name)
         adapter.add_message_handler(parameters, message_handler)
 
