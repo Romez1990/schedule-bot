94855ef1dc55006aaca2f4979175df4a0e11cc86
Romez <Romez1990@gmail.com>
2022-07-08 11:33:19 +0300

    temp

--- src/messenger_services/messenger_service/messenger_controller_registrar_impl.py
+++ src/messenger_services/messenger_service/messenger_controller_registrar_impl.py
@@ -1,28 +1,29 @@
 from typing import (
     Sequence,
+    Callable,
     Type,
+    Awaitable,
 )
 
 from infrastructure.ioc_container import service, Container
 from messenger_services.telegram_service import TelegramService
 from messenger_services.vk_service import VkService
 from .structures import (
+    Message,
     MessageHandlerParameters,
 )
 from .messenger_controller_registrar import MessengerControllerRegistrar
-from .controller_decorator import messenger_controllers
-from .message_handler_adapter import MessageHandlerAdapter
-from .messenger_controller import MessengerController
 from .messenger_service import MessengerService
+from .messenger_adapter import MessengerAdapter
+from .messenger_controller import MessengerController
+from .controller_decorator import messenger_controllers
 from .message_handler_decorator import message_handler_parameters
 
 
 @service
 class MessengerControllerRegistrarImpl(MessengerControllerRegistrar):
-    def __init__(self, message_handler_adapter: MessageHandlerAdapter,
-                 telegram: TelegramService,
+    def __init__(self, telegram: TelegramService,
                  vk: VkService) -> None:
-        self.__message_handler_adapter = message_handler_adapter
         self.messenger_services: Sequence[MessengerService] = [
             telegram,
             vk,
@@ -55,6 +56,15 @@ class MessengerControllerRegistrarImpl(MessengerControllerRegistrar):
             self, messenger_service: MessengerService, controller: MessengerController,
             parameters: MessageHandlerParameters) -> None:
         adapter = messenger_service.adapter
-        message_handler = self.__message_handler_adapter.get_message_handler(controller, adapter,
-                                                                             parameters.method_name)
+        message_handler = self.__get_message_handler(controller, adapter, parameters.method_name)
         adapter.add_message_handler(parameters, message_handler)
+
+    def __get_message_handler(self, controller: MessengerController, adapter: MessengerAdapter,
+                              method_name: str) -> Callable[[object], Awaitable[None]]:
+        method: Callable[[MessengerController, Message], Awaitable[None]] = getattr(controller, method_name)
+
+        async def message_handler(messenger_message: object) -> None:
+            message = adapter.map_message(messenger_message)
+            await method(controller, message)
+
+        return message_handler
