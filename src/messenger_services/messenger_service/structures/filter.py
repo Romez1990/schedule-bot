664b713979edb08bc86825e991882f87084af009
Romez <Romez1990@gmail.com>
2022-07-10 09:32:20 +0300

    temp

--- src/messenger_services/messenger_service/structures/filter.py
+++
@@ -1,8 +0,0 @@
-from abc import ABCMeta, abstractmethod
-
-from .message import Message
-
-
-class Filter(metaclass=ABCMeta):
-    @abstractmethod
-    def check(self, message: Message) -> bool: ...
